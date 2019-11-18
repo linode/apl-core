a1d16eafb8c5dc14f68970ac0ffb2a7ac3d1db90
Maurice Faber <morriz@idiotz.nl>
2019-11-18 15:52:15 +0100

    fixed skeleton chart

--- bin/skeleton-chart.sh
+++ bin/skeleton-chart.sh
@@ -11,13 +11,13 @@ args=${@:-''}
 echo "Creating tmp chart from skeleton with manifests from: k8s/apps/$app"
 # just copy the sources temporarily to /tmp if not there yet
 [ -f "/tmp/charts/$app" ] && rm -rf /tmp/charts/$app
-mkdir -p /tmp/charts/$app
+mkdir -p /tmp/charts/$app/templates
 cp -r $root/charts/skeleton/* /tmp/charts/$app/
 sed -i -e "s/##CHART/$app/g" /tmp/charts/$app/Chart.yaml
-printf "" > /tmp/charts/$app/all.yaml
+printf "" >/tmp/charts/$app/templates/all.yaml
 if [ "$args" != "" ]; then
-  kubectl apply $args -f $root/k8s/apps/$app --dry-run -o yaml > /tmp/charts/$app/all.yaml
+  kubectl apply $args -f $root/k8s/apps/$app --dry-run -o yaml >/tmp/charts/$app/templates/all.yaml
 else
-  find $root/k8s/apps/$app/* -type f -name "*.yaml" -exec sh -c "cat {}; printf '\n---\n'" >> /tmp/charts/$app/all.yaml \;
+  find $root/k8s/apps/$app/* -type f -name "*.yaml" -exec sh -c "cat {}; printf '\n---\n'" \; >>/tmp/charts/$app/templates/all.yaml
 fi
 echo "manifests:"
