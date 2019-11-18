ce8b128baae85aa88c8188f1ff26cd5331a9ff5b
Maurice Faber <morriz@idiotz.nl>
2019-11-18 12:07:48 +0100

    upgraded knative

--- bin/create-gke-cluster.sh
+++ bin/create-gke-cluster.sh
@@ -4,7 +4,7 @@
 
 # create the cluster
 gcloud beta container --project "$GCP_PROJECT" clusters create "$CLUSTER" --region "$REGION" \
-  --no-enable-basic-auth --cluster-version "1.13.7-gke.8" --machine-type "n1-standard-4" \
+  --no-enable-basic-auth --cluster-version "1.14.8-gke.12" --machine-type "n1-standard-4" \
   --image-type "COS" --disk-type "pd-standard" --disk-size "100" --node-labels customer=$CUSTOMER \
   --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/cloud-platform" \
   --max-pods-per-node "110" --num-nodes "1" --no-enable-cloud-logging --no-enable-cloud-monitoring \
@@ -15,7 +15,7 @@ gcloud beta container --project "$GCP_PROJECT" clusters create "$CLUSTER" --regi
   --maintenance-window "01:00" --labels customer=$CUSTOMER --enable-tpu \
   --enable-autoprovisioning --min-cpu 1 --max-cpu 8 --min-memory 8 --max-memory 32 \
   --resource-usage-bigquery-dataset "$METERING_SET" --enable-network-egress-metering --enable-resource-consumption-metering
-  # --enable-pod-security-policy
+# --enable-pod-security-policy
 
 gcloud container clusters get-credentials $CLUSTER --region $REGION --project $GCP_PROJECT
 
