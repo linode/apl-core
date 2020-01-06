bfb875cb5720aaf6b5fa6b155cc7a41b67ffeb85
Maurice Faber <morriz@idiotz.nl>
2020-01-06 19:37:02 +0100

    upgraded to use helm 3

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,16 @@
 # /bin/bash
 shopt -s expand_aliases
 . bin/aliases
-. .env
+. .env/cloud
 
 set -e
 
-context=$CONTEXT_PREFIX-$STAGE
-
 if [ ! -d ~/.kube ]; then
   echo "Creating kube config"
-  k config set-context $context --server=$CLUSTER_API_HOST --insecure-skip-tls-verify=true
-  k config set-credentials $context --token="$KUBE_TOKEN"
-  k config set-context $context --context=$context
+  k config set-context $K8S_CONTEXT --server=$CLUSTER_API_HOST --insecure-skip-tls-verify=true
+  k config set-credentials $K8S_CONTEXT --token="$KUBE_TOKEN"
+  k config set-context $K8S_CONTEXT --context=$K8S_CONTEXT
 fi
 
-kcu $context
+kcu $K8S_CONTEXT
 hf -e $CLOUD-$STAGE apply --concurrency=1 --skip-deps
