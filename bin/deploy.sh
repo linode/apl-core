0123648a23552f64af78a928684f2a1e20aebdaa
Maurice Faber <morriz@idiotz.nl>
2020-02-06 21:53:07 +0100

    fixes

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,10 +11,9 @@ if [ ! -d ~/.kube ]; then
   k config set-context $K8S_CONTEXT --context=$K8S_CONTEXT
 fi
 
-kcu $K8S_CONTEXT
-
+hf repos
 # install some stuff that we never want to end up as charts
 # (might get corrupted and we can then never pass that stage of deployment)
 hft -f helmfile.tpl/helmfile-init.yaml | k apply -f -
 k apply --validate=false -f k8s/cert-manager-init
-hf apply
+hfd apply
