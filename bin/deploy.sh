dc53f7901a41ececb1b5104b854ba9ad85002b21
Maurice Faber <morriz@idiotz.nl>
2020-02-07 02:44:33 +0100

    added sync.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,10 +4,12 @@ shopt -s expand_aliases
 
 set -e
 
-# install some stuff that we never want to end up as charts
-# (might get corrupted and we can then never pass that stage of deployment)
-hft -f helmfile.tpl/helmfile-init.yaml | k apply -f -
-k apply --validate=false -f k8s/cert-manager-init
+if [ ! -z $1 ]; then
+  # install some stuff that we never want to end up as charts
+  # (might get corrupted and we can then never pass that stage of deployment)
+  hft -f helmfile.tpl/helmfile-init.yaml | k apply -f -
+  k apply --validate=false -f k8s/cert-manager-init
+fi
 
 # now the charts
 hfd apply
---
+++ bin/sync.sh
@@ -0,0 +1,2 @@
+# /bin/bash
+bin/deploy.sh 1
