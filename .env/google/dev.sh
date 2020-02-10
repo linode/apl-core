9e9e01d96a1869a5305f4b67dac3e3a559ca9554
Maurice Faber <morriz@idiotz.nl>
2020-02-10 18:34:53 +0100

    renamed gks to gke [ci skip]

--- .env/google/dev.sh
+++ .env/google/dev.sh
@@ -1,11 +1,10 @@
 # first part used for bin/*-gke.sh:
 export GOOGLE_REGION="europe-west4"
 export PROJECT="otomi-cloud"
-export CLUSTER_NAME="otomi-gks"
+export CLUSTER_NAME="otomi-gke-${CLUSTER}"
 export DNS_NAME="otomi.cloud"
 export DNS_ZONE="otomi"
 export METERING_SET="otomi_metering"
 
 # used for stack deployment:
-export CLUSTER_API_HOST="https://34.90.97.189"
-export K8S_CONTEXT="gke_${PROJECT}_${GOOGLE_REGION}_${CLUSTER_NAME}-${CLUSTER}"
+export K8S_CONTEXT="gks_${PROJECT}_${GOOGLE_REGION}_${CLUSTER_NAME}"
