21cfca7c689be899a5ee1736581d7a475daef00c
Maurice Faber <morriz@idiotz.nl>
2020-02-19 02:54:41 +0100

    separated stack from values, see otomi-values repo

--- bin/env.sh
+++ bin/env.sh
@@ -1,14 +1,15 @@
 export CLUSTER=${CLUSTER:-dev}
+export ENV_DIR=${ENV_DIR:-$PWD/env}
 . bin/colors.sh
 
-cd .env >/dev/null
+cd $ENV_DIR >/dev/null
 c=(*/)
 cd - >/dev/null
 clouds="${c[@]%/}"
 if [[ ! $clouds == *$CLOUD* ]]; then
   printf "${COLOR_RED}ERROR: The value of CLOUD env must be one of the following: ${COLOR_YELLOW}$clouds${COLOR_NC}\n"
 else
-  cd .env/$CLOUD >/dev/null
+  cd $ENV_DIR/$CLOUD >/dev/null
   c=(*.sh)
   cd - >/dev/null
   clusters="${c[@]%.sh}"
@@ -17,4 +18,4 @@ else
   fi
 fi
 # source dependent env vars
-. $PWD/.env/$CLOUD/$CLUSTER.sh
+. $ENV_DIR/$CLOUD/$CLUSTER.sh
--- bin/gen-drone.sh
+++ bin/gen-drone.sh
@@ -1,14 +1,15 @@
 # /bin/bash
+export ENV_DIR=${ENV_DIR:-$PWD/env}
 
-tpl="$PWD/.drone.yaml"
+tpl="$ENV_DIR/.drone.tpl.yaml"
 
-cd .env >/dev/null
+cd $ENV_DIR >/dev/null
 for c in */; do
   CLOUD=$(echo $c | sed -e 's/\///g')
   cd $CLOUD >/dev/null
   for c in *.sh; do
     CLUSTER=$(echo $c | sed -e 's/\.sh//g')
-    echo "creating .env/$CLOUD/$CLUSTER/.drone.yml"
+    echo "creating $ENV_DIR/$CLOUD/$CLUSTER/.drone.yml"
     cat $tpl | sed -e "s/CCCLOUD/${CLOUD}/g" -e "s/CCCLUSTER/${CLUSTER}/g" >.drone.yml
   done
   cd - >/dev/null
