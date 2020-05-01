995b35b4c77021c1c911e27025f493afcdb93ced
Maurice Faber <morriz@idiotz.nl>
2020-05-01 16:43:10 +0200

    fix: removed faulty stack maintenance task [ci skip]

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -3,11 +3,7 @@ shopt -s expand_aliases
 . bin/utils.sh
 set -e
 
-printf "${COLOR_LIGHT_PURPLE}Killing istio-pilot pod...${COLOR_NC}\n"
-kis delete po $(kis get po -l "istio=pilot,app=pilot" -ojsonpath='{.items[0].metadata.name}')
 if [ "$CLOUD" = "azure" ]; then
   printf "${COLOR_LIGHT_PURPLE}DONE!\nKilling ingress-azure pod...${COLOR_NC}\n"
   ki delete po $(ki get po -l "app=ingress-azure" -ojsonpath='{.items[0].metadata.name}')
 fi
-# printf "${COLOR_LIGHT_PURPLE}DONE!\nKilling loki pod. This may take some time...${COLOR_NC}\n"
-# km delete po $(km get po -l "app=loki" -ojsonpath='{.items[0].metadata.name}')
