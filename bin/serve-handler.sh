4f6730269b80e2203c1da7071e1060f12a45218f
Maurice Faber <morriz@idiotz.nl>
2020-07-06 22:05:32 +0200

    fix: health check

--- bin/serve-handler.sh
+++ bin/serve-handler.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 ok="HTTP/1.0 200 OK\r\nContent-length: 2\r\n\r\nOK\r\n"
 err="HTTP/1.0 500 Internal Server Error\r\nContent-length: 21\r\n\r\nInternal Server Error\r\n"
-mode=
-while IFS= read -r line && [ "$mode" = "" ]; do
+request=
+while IFS= read -r line && [ "$request" = "" ]; do
   if [[ $line = "GET /"* ]]; then
     request=${line#* } # delete everything up to first space in $line
-    mode=${request:1:4}
   fi
 done
-if [ "$mode" = " " ]; then printf "$ok" && exit; fi
+# health check?
+if [ "${request:1:1}" = " " ]; then printf "$ok" && exit; fi
+# other request:
+mode=${request:1:4}
 if [ "$mode" != "dec " ] && [ "$mode" != "enc " ]; then
   echo "No valid request: '$mode'. Should be one of: dec|enc" >&2
   printf "$err"
