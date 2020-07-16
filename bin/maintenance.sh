ea7bcabc4b7abde3c3c8fa45d8c26ff3d62560d5
Maurice Faber <morriz@idiotz.nl>
2020-07-16 18:09:19 +0200

    fix: maintenance error [ci skip]

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -7,6 +7,3 @@ if [ "$CLOUD" = "azure" ]; then
   printf "${COLOR_LIGHT_PURPLE}DONE!\nKilling ingress-azure pod...${COLOR_NC}\n"
   ki delete po $(ki get po -l "app=ingress-azure" -ojsonpath='{.items[0].metadata.name}')
 fi
-
-# move cluster wildcard secret to harbor namespace
-kis get secret | grep -E "harbor-$CLUSTER-" | awk '{print $1}' | xargs kubectl -n istio-system get secret --export -o yaml | k -n harbor apply -f -
