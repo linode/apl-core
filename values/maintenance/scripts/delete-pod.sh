512421061a43153a247f660cdb178f78b203eaef
Jehoszafat Zimnowoda <jzimnowo@gmail.com>
2020-07-29 16:23:27 +0200

    feat: add jobs to maintenance chart

---
+++ values/maintenance/scripts/delete-pod.sh
@@ -0,0 +1,8 @@
+set -e 
+[[ -z "$LABELS" || -z "${NS}" ]] && exit 1
+
+echo "MAINTENANCE TASK: deleting first found pod with labels ${LABELS} in namespace ${NS}"
+echo "MAINTENANCE NOTE: this is needed because the component in question does not function correctly and loses desired state."
+echo "MAINTENANCE TODO: find out if and why this is the case and keep filing reports for component with labels ${LABELS} in namespace ${NS}"
+pod=$(kubectl -n ${NS} get po -l "${LABELS}" -ojsonpath='{.items[0].metadata.name}')
+kubectl -n ${NS} delete po $pod
