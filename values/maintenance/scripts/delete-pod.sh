d005820f1a5e2ff5eb36b0f4572ddfe6a339bd30
Jehoszafat Zimnowoda <jzimnowo@gmail.com>
2020-07-30 10:38:57 +0200

    feat: print error message to indicate missing env variables

--- values/maintenance/scripts/delete-pod.sh
+++ values/maintenance/scripts/delete-pod.sh
@@ -1,6 +1,6 @@
-set -e 
-[[ -z "$LABELS" || -z "${NS}" ]] && exit 1
+[[ -z "$LABELS" || -z "$NS" ]] && echo "Error: Missing environment variables" && exit 2
 
+set -e
 echo "MAINTENANCE TASK: deleting first found pod with labels ${LABELS} in namespace ${NS}"
 echo "MAINTENANCE NOTE: this is needed because the component in question does not function correctly and loses desired state."
 echo "MAINTENANCE TODO: find out if and why this is the case and keep filing reports for component with labels ${LABELS} in namespace ${NS}"
