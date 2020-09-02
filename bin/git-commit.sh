12953af6c55addf54813ea0e6a953b509cfa3b00
Jehoszafat Zimnowoda <jzimnowo@gmail.com>
2020-09-02 13:35:11 +0200

    fix: add env.ini

--- bin/git-commit.sh
+++ bin/git-commit.sh
@@ -7,6 +7,7 @@ match=".drone.tpl.yaml\|env.ini"
 if git -C $ENV_DIR diff --name-only | grep $match >/dev/null; then
   bin/gen-drone.sh
   git -C $ENV_DIR add $ENV_DIR/*/.drone.*.yml
+  git -C $ENV_DIR add env.ini
 fi
 bin/crypt.sh enc
 git -C $ENV_DIR add $ENV_DIR/*.yaml
