cf68e627f24e5c1b7dc78c389d8b3829e6d21132
Martijn van Calker <martijn.vancalker@redkubes.com>
2020-09-09 14:32:12 +0200

    task: confirm current-context before applying cluster changes

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,12 @@ shopt -s expand_aliases
 . bin/utils.sh
 set -e
 
+read -p "Current kubectl context: $(kubectl config current-context), are you sure to apply changes (Y/n)? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+    exit 1
+fi
+
 # install some stuff that we never want to end up as charts
 hft -f helmfile.tpl/helmfile-init.yaml | k apply -f -
 # not ready yet:
