a692a6470a0c79bc14f7f3bdc82ffe6fa09056f3
Martijn van Calker <martijn.vancalker@redkubes.com>
2020-09-14 11:06:38 +0200

    task: use jq to query for version

--- bin/utils.sh
+++ bin/utils.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 printf "${COLOR_LIGHT_PURPLE}Loading environment...${COLOR_NC}\n"
-PACKAGE_VERSION=$(cat package.json |
-  grep '"version"' |
-  head -1 |
-  awk -F: '{ print $2 }' |
-  sed 's/[",]//g' |
-  tr -d '[[:space:]]')
+PACKAGE_VERSION=$(cat package.json | jq -r .version)
 
 . bin/env.sh
 noEnvError=$?
