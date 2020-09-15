c1c34b55c994cdc49040b3e16b637e67c3960d52
Maurice Faber <maurice.faber@redkubes.com>
2020-09-15 16:26:33 +0200

    fix: failing otomi version check for new users [ci skip]

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-[[ "${BASH_SOURCE[0]}" != "${0}" ]] && echo "Sourcing env..." && SOURCING=true
 shopt -s expand_aliases
 . bin/aliases
 set -e
@@ -9,40 +8,38 @@ ENV_DIR=${ENV_DIR:-./env}
 otomi_version=$(otomi_version)
 alias otomi="${ENV_DIR}/bin/otomi"
 
-if [ ! $SOURCING ]; then
-  skip_demo_files=$1
-  cid=''
-  cmd_cp='cp -r'
-  cp_path=''
-  [ -f $ENV_DIR/bin/otomi ] && has_otomi=true
+skip_demo_files=$1
+cid=''
+cmd_cp='cp -r'
+cp_path=''
+[ -f $ENV_DIR/bin/otomi ] && has_otomi=true
 
-  # install CLI
-  otomi_path="${ENV_DIR}/bin/"
-  mkdir -p $otomi_path &>/dev/null
-  img="eu.gcr.io/otomi-cloud/otomi-stack:v${otomi_version}"
-  echo "Installing artifacts from ${img}"
-  if [ "$IN_DOCKER" == "0" ]; then
-    cid="$(docker create $img)"
-    cp_path="${cid}:"
-    cmd_cp="docker cp"
-  fi
-  $cmd_cp ${cp_path}/home/app/stack/bin/aliases $otomi_path
-  $cmd_cp ${cp_path}/home/app/stack/bin/otomi $otomi_path
-  $cmd_cp ${cp_path}/home/app/stack/.values/.vscode $ENV_DIR/
-  for f in '.gitattributes' '.sops.yaml'; do
-    [ ! -f $ENV_DIR/$f ] && $cmd_cp ${cp_path}/home/app/stack/.values/$f $ENV_DIR/
-  done
-  for f in '.gitignore' '.prettierrc.yml' 'README.md'; do
-    $cmd_cp ${cp_path}/home/app/stack/.values/$f $ENV_DIR/
-  done
-  if [ "$skip_demo_files" != "1" ]; then
-    echo "Installing demo files"
-    $cmd_cp ${cp_path}/home/app/stack/.demo $ENV_DIR/env
-  fi
-  [ ! -z "$cid" ] && docker rm ${cid} >/dev/null
-  if [ ! $has_otomi ]; then
-    echo "You can now use otomi CLI"
-    echo "Start by sourcing aliases:"
-    echo ". bin/aliases"
-  fi
+# install CLI
+otomi_path="${ENV_DIR}/bin/"
+mkdir -p $otomi_path &>/dev/null
+img="eu.gcr.io/otomi-cloud/otomi-stack:v${otomi_version}"
+echo "Installing artifacts from ${img}"
+if [ "$IN_DOCKER" == "0" ]; then
+  cid="$(docker create $img)"
+  cp_path="${cid}:"
+  cmd_cp="docker cp"
+fi
+$cmd_cp ${cp_path}/home/app/stack/bin/aliases $otomi_path
+$cmd_cp ${cp_path}/home/app/stack/bin/otomi $otomi_path
+$cmd_cp ${cp_path}/home/app/stack/.values/.vscode $ENV_DIR/
+for f in '.gitattributes' '.sops.yaml'; do
+  [ ! -f $ENV_DIR/$f ] && $cmd_cp ${cp_path}/home/app/stack/.values/$f $ENV_DIR/
+done
+for f in '.gitignore' '.prettierrc.yml' 'README.md'; do
+  $cmd_cp ${cp_path}/home/app/stack/.values/$f $ENV_DIR/
+done
+if [ "$skip_demo_files" != "1" ]; then
+  echo "Installing demo files"
+  $cmd_cp ${cp_path}/home/app/stack/.demo $ENV_DIR/env
+fi
+[ ! -z "$cid" ] && docker rm ${cid} >/dev/null
+if [ ! $has_otomi ]; then
+  echo "You can now use otomi CLI"
+  echo "Start by sourcing aliases:"
+  echo ". bin/aliases"
 fi
