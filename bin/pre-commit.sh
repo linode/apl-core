f4f5e5b17884a05654137220d16da17a9a5de36e
Maurice Faber <maurice.faber@redkubes.com>
2020-09-15 09:48:18 +0200

    fix: pipeline [ci skip]

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -2,8 +2,10 @@
 set -e
 
 ENV_DIR=${ENV_DIR:-./env}
+RECEIVER=$(cat $ENV_DIR/settings.yaml | yq r - alerts.receiver)
 
-match=".drone.tpl.yaml\|env.ini"
+export RECEIVER
+match=".drone.tpl.$receiver.yaml\|clusters.yaml"
 if git -C $ENV_DIR diff --name-only | grep $match >/dev/null; then
   bin/gen-drone.sh
 fi
