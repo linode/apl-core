7627c94edabd24aa54d72f1925b4db11d2e0ad48
Maurice Faber <maurice.faber@redkubes.com>
2020-09-17 12:15:47 +0200

    fix: regression, sorry

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
+shopt -s expand_aliases
 . bin/aliases
 set -e
 set -o pipefail
 
 # install some stuff that we never want to end up as charts
-helmfile -e $CLOUD-$CLUSTER template -f helmfile.tpl/helmfile-init.yaml | kubectl apply -f -
+otomi template -f helmfile.tpl/helmfile-init.yaml | kubectl apply -f -
 # not ready yet:
 # set +e
 # k -n maintenance create secret generic flux-ssh --from-file=identity=.ssh/id_rsa &>/dev/null
 # set -e
 kubectl apply -f charts/gatekeeper-operator/crds
 kubectl apply -f charts/prometheus-operator/crds
-
-# now sync
-helmfile -e $CLOUD-$CLUSTER $@ apply --skip-deps
