f5dcebb256eaf809e51e5877310f868a01db1b41
Maurice Faber <maurice.faber@redkubes.com>
2020-09-17 13:02:53 +0200

    fix: regression deploy [ci skip]

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,11 +2,14 @@
 set -e
 set -o pipefail
 
+hf="helmfile -e $CLOUD-$CLUSTER"
 # install some stuff that we never want to end up as charts
-helmfile -e $CLOUD-$CLUSTER -f helmfile.tpl/helmfile-init.yaml template | kubectl apply -f -
+$hf -f helmfile.tpl/helmfile-init.yaml template | kubectl apply -f -
 # not ready yet:
 # set +e
 # k -n maintenance create secret generic flux-ssh --from-file=identity=.ssh/id_rsa &>/dev/null
 # set -e
 kubectl apply -f charts/gatekeeper-operator/crds
 kubectl apply -f charts/prometheus-operator/crds
+
+$hf apply
