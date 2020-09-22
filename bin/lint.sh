25fea5f57194718d22ecd6054f014501040f3725
Alin Spinu <spinualin@gmail.com>
2020-09-22 17:10:44 +0300

    fix: add fix running kubeval from script

--- bin/lint.sh
+++ bin/lint.sh
@@ -2,9 +2,10 @@
 
 # accepts a list of versions to check against
 # example:~$ otomi kubeval 1.15.0 1.16.0 1.17.0
-local k8s_versions=("${@:-1.16.0}")
-local tmp_validation_dir=/tmp/kubeval-fixtures
-local exitcode=0
+k8s_versions=("${@:-1.16.0}")
+tmp_validation_dir=/tmp/kubeval-fixtures
+exitcode=0
+hf="helmfile -e $CLOUD-$CLUSTER"
 
 clean_exit() {
     [[ $exitcode -eq 0 ]] && echo "Validation Success" || echo "Validation Failed"
@@ -14,11 +15,12 @@ clean_exit() {
 
 echo "Generating Manifests in tmp location."
 # using OUTPUT-DIR parameter because kubeval is not accepting multiple resources per file
-drun helmfile -e $CLOUD-$CLUSTER --quiet template --skip-deps --output-dir=$tmp_validation_dir >/dev/null
+$hf --quiet template --skip-deps --output-dir=$tmp_validation_dir >/dev/null
+
 for version in "${k8s_versions[@]}"; do
     echo "Validating Otomi Stack against Kubernetes Version: $version"
     (
-        drun kubeval --force-color -d $tmp_validation_dir --strict --ignore-missing-schemas --kubernetes-version $version
+        kubeval --force-color -d $tmp_validation_dir --strict --ignore-missing-schemas --kubernetes-version $version
     ) || exitcode=1
 done
 clean_exit
