954734361fb7afc5dc690420233aa44c2114ff90
Martijn van Calker <martijn.vancalker@redkubes.com>
2020-09-23 13:55:36 +0200

    wip/poc: Replace encrypt/decrypt with k8s-jobs

--- bin/server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ "$GCLOUD_SERVICE_KEY" = "" ]; then
-  echo 'GCLOUD_SERVICE_KEY not set!'
-  exit 1
-fi
-echo "Starting netcat server at port 17771"
-while true; do
-  nc -l -p 17771 -e bin/serve-handler.sh
-done
