a4a86025db19de1752ba0f872c60102614f974de
Maurice Faber <maurice.faber@redkubes.com>
2020-09-25 12:26:23 +0200

    fix: last bits [ci skip]

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,10 +5,6 @@ set -o pipefail
 hf="helmfile -e $CLOUD-$CLUSTER"
 # install some stuff that we never want to end up as charts
 $hf -f helmfile.tpl/helmfile-init.yaml template | kubectl apply -f -
-# not ready yet:
-# set +e
-# k -n maintenance create secret generic flux-ssh --from-file=identity=.ssh/id_rsa &>/dev/null
-# set -e
 kubectl apply -f charts/gatekeeper-operator/crds
 kubectl apply -f charts/prometheus-operator/crds
 
