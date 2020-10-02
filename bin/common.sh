a69da7ff19e45f389b939cab056743f957470288
Maurice Faber <maurice.faber@redkubes.com>
2020-10-02 23:45:38 +0200

    chore(release): 0.11.26

--- bin/common.sh
+++ bin/common.sh
@@ -1,4 +1,5 @@
 function otomi_image_tag() {
+  [ "$ENV_DIR" == "" ] && echo 'latest' && exit
   local version
   local clusters_file="$ENV_DIR/env/clusters.yaml"
   if [ -f $clusters_file ] && [ "$CLOUD" != "" ] && [ "$CLUSTER" != "" ]; then
@@ -11,19 +12,18 @@ function otomi_image_tag() {
 }
 
 function customer_name() {
+  [ "$ENV_DIR" == "" ] && exit 1
   cat $ENV_DIR/env/settings.yaml | yq r - customer.name
 }
 
 function get_receiver() {
-  set -x
+  [ "$ENV_DIR" == "" ] && exit 1
   file=$ENV_DIR/env/secrets.settings.yaml
   receiver=$(cat $file | yq r - alerts.receiver)
   if [ "$receiver" == "" ] && [ -f "$file.dec" ]; then
     receiver=$(cat $file.dec | yq r - alerts.receiver)
   fi
   if [ "$receiver" == "" ]; then
-    pwd
-    ls -als $file
     helm secrets dec $file
     receiver=$(cat $file.dec | yq r - alerts.receiver)
   fi
