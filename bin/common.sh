655a919fd7714508fb587b227d2b87d40b5d7ab9
Maurice Faber <maurice.faber@redkubes.com>
2020-10-03 01:54:07 +0200

    chore(release): 0.11.26

--- bin/common.sh
+++ bin/common.sh
@@ -18,20 +18,20 @@ function customer_name() {
 
 function get_receiver() {
   [ "$ENV_DIR" == "" ] && exit 1
+  prepare_crypt
   file=$ENV_DIR/env/settings.yaml
   file_secrets=$ENV_DIR/env/secrets.settings.yaml
-  receiver=$(cat $file | yq r - alerts.receiver)
-  if [ "$receiver" == "" ]; then
-    if [ ! -f "$file_secrets.dec" ]; then
-      set +e
-      helm secrets dec $file_secrets >/dev/null
-      set -e
-    fi
-    receiver=$(cat $file_secrets.dec | yq r - alerts.receiver)
-    [ "$receiver" != "" ] && [ "$@" != "" ] && val=$(cat $file_secrets.dec | yq r - alerts.$receiver.$@)
+  if [ ! -f "$file_secrets.dec" ]; then
+    set +e
+    helm secrets dec $file_secrets >/dev/null
+    set -e
   fi
+  receiver=$(cat $file | yq r - alerts.receiver)
+  [ "$receiver" == "" ] && receiver=$(cat $file_secrets.dec | yq r - alerts.receiver)
   [ "$receiver" == "" ] && exit 1
-  if [ "$val" != "" ]; then
+  if [ "$@" != "" ]; then
+    val=$(cat $file_secrets.dec | yq r - alerts.$receiver.$@)
+    [ "$val" == "" ] && exit 1
     echo $val
   else
     echo $receiver
