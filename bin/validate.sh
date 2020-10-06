a6a0c58ca3e83a228468f412f036d7250a1ff5f3
jeho <jehoszafat.zimnowoda@redkubes.com>
2020-10-07 01:04:40 +0200

    Schema validation (#156)
    * fix: print error message on missing env
    
    * build: clean /tmp directory
    
    * build: add ajv tool for values against jsonschema validation
    
    * fix: schema regexp and oneOf usage
    
    * feat: add otomi validate command
    
    * fix: remove not existing properties
    
    * fix: add missing schemas
    
    * feat: validate all clusters
    
    * fix: make entrypoint optional
    
    * docs: document function
    
    * feat: validate values for each cluster
    
    * feat: generate loose schema
    
    * fix: define enums for k8sversion
    
    Co-authored-by: Maurice Faber <maurice.faber@redkubes.com>

---
+++ bin/validate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+values_path="/tmp/values-$CLOUD-$CLUSTER.yaml"
+
+function cleanup {
+  [[ "$MOUNT_TMP_DIR" != "1" ]] && rm -f $values_path
+  return 0;
+}
+
+trap cleanup EXIT
+. bin/common.sh
+
+prepare_crypt
+helmfileOutputHide="(^\W+$|skipping|basePath=|Decrypting)"
+helmfileOutputHideTpl="(^[\W^-]+$|skipping|basePath=|Decrypting)"
+
+helmfile -e $CLOUD-$CLUSTER -f helmfile.tpl/helmfile-dump.yaml build | grep -Ev $helmfileOutputHide | sed -e 's@../env@'"${ENV_DIR}"'@g' | \
+  yq read -P - 'releases[0].values[0]' > $values_path
+ajv validate -s './values-schema.yaml' -d $values_path
