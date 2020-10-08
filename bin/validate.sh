748cde5986953784a5f1413854594a5f3f97651e
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2020-10-08 14:18:29 +0200

    improvement: use quiet flag for helmfile

--- bin/validate.sh
+++ bin/validate.sh
@@ -6,17 +6,19 @@ set -o pipefail
 values_path="/tmp/values-$CLOUD-$CLUSTER.yaml"
 
 function cleanup {
+  local exitcode=$?
   [[ "$MOUNT_TMP_DIR" != "1" ]] && rm -f $values_path
-  return 0;
+  return $exitcode;
 }
 
 trap cleanup EXIT
 . bin/common.sh
 
+[ "$VERBOSE" == "0" ] && quiet='--quiet'
 prepare_crypt
 helmfileOutputHide="(^\W+$|skipping|basePath=|Decrypting)"
 helmfileOutputHideTpl="(^[\W^-]+$|skipping|basePath=|Decrypting)"
 
-helmfile -e $CLOUD-$CLUSTER -f helmfile.tpl/helmfile-dump.yaml build | grep -Ev $helmfileOutputHide | sed -e 's@../env@'"${ENV_DIR}"'@g' | \
+helmfile $quiet -e $CLOUD-$CLUSTER -f helmfile.tpl/helmfile-dump.yaml build | grep -Ev $helmfileOutputHide | sed -e 's@../env@'"${ENV_DIR}"'@g' | \
   yq read -P - 'releases[0].values[0]' > $values_path
 ajv validate -s './values-schema.yaml' -d $values_path
