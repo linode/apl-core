c4741fa926647a35d3eb5378dac404474028d587
Alin Spinu <spinualin@gmail.com>
2020-10-13 15:31:24 +0300

    feat: wip linting task

--- bin/lint.sh
+++ bin/lint.sh
@@ -4,119 +4,89 @@
 # example:~$ otomi lint
 set -e
 set -o pipefail
-k8sResourcesPath="/tmp/kubeval-fixtures"
-# schemaOutputPath="/tmp/generated-crd-schemas/schemas"
-outputPath="/tmp/generated-crd-schemas"
-destinationFile="$outputPath/all.json"
 
-queryExpression=$(mktemp -u)
-trap 'rm -f -- "$queryExpression"' INT TERM HUP ERR EXIT
-exitcode=1
+schemaOutputPath="/tmp/kubernetes-json-schema/master"
+outputPath="/tmp/generated-crd-schemas"
+schemasBundleFile="$outputPath/all.json"
+k8sResourcesPath="/tmp/kubeval-fixtures"
+extractCrdSchemaJQFile=$(mktemp -u)
+trap 'rm -f -- "$extractCrdSchemaJQFile" ' INT TERM HUP ERR EXIT
 hf="helmfile -e $CLOUD-$CLUSTER"
 ENV_DIR=${ENV_DIR:-$PWD}
+exitcode=1
 
 . bin/common.sh
 
-declare -a allowedVersions=(
-    v1.16.0
-    v1.17.0
-    v1.18.0
-)
+version="v$(get_k8s_version).0"
 
 cleanup() {
     [[ $exitcode -eq 0 ]] && echo "Validation Success" || echo "Validation Failed"
-    # rm -rf $k8sResourcesPath $outputPath
+    rm -rf $k8sResourcesPath $outputPath
     exit $exitcode
 }
 trap cleanup EXIT
 
 run_setup() {
     mkdir -p $outputPath $schemaOutputPath $k8sResourcesPath
-    cat <<'EOF' >$queryExpression
+    echo "" >$schemasBundleFile
+    tar -xzf ".schemas/${version}-standalone.tar.gz" -C $schemaOutputPath
+
+    # @TODO  for loop over .spec.versions[] and generate one file for each version
+    cat <<'EOF' >$extractCrdSchemaJQFile
     {
-        properties: .spec.validation.openAPIV3Schema.properties,
-        description: .spec.validation.openAPIV3Schema.description,
-        required: .spec.validation.openAPIV3Schema.required,
-        title: .metadata.name,
-        type: "object",
-        "$schema": "http://json-schema.org/draft/2019-09/schema#",
-        "x-kubernetes-group-version-kind.group": .spec.group,
-        "x-kubernetes-group-version-kind.kind": .spec.names.kind,
-        "x-kubernetes-group-version-kind.version": .spec.version
+        filename: ( (.spec.names.kind | ascii_downcase) +"-"+  (.spec.group | split(".")[0]) +"-"+ ( .spec.versions[0].name // .spec.version ) + ".json" ),
+        schema: {
+            properties: .spec.validation.openAPIV3Schema.properties,
+            description: (.spec.validation.openAPIV3Schema.description // ""),
+            required: (.spec.validation.openAPIV3Schema.required // []),
+            title: .metadata.name,
+            type: "object",
+            "$schema": "http://json-schema.org/draft/2019-09/schema#",
+            "x-kubernetes-group-version-kind.group": .spec.group,
+            "x-kubernetes-group-version-kind.kind": .spec.names.kind,
+            "x-kubernetes-group-version-kind.version": ( .spec.versions[0].name // .spec.version )
+        }
     }
 EOF
 }
 
-filter_crds() {
+process_crd() {
     local document="$1"
-    local filterExpr='select(.kind=="CustomResourceDefinition" and .spec.validation.openAPIV3Schema.properties != null)'
+    local filterCRDExpr='select(.kind=="CustomResourceDefinition" and .spec.validation.openAPIV3Schema.properties != null)'
     echo "$(date --utc) Processing: $document"
-
-    yq r -d'*' -j "$document" |
-        jq -c "$filterExpr" |
-        jq -S -c --raw-output -f "$queryExpression" >>"$destinationFile"
+    {
+        yq r -d'*' -j "$document" |
+            jq -c "$filterCRDExpr" |
+            jq -S -c --raw-output -f "$extractCrdSchemaJQFile" >>"$schemasBundleFile"
+    } || {
+        echo "$(date --utc) ERROR Processing: $document"
+    }
 }
 
-generate_canonical_schemas() {
-    echo "$(date --utc) Compiling all json-schemas from: $destinationFile"
+run_setup
 
-    # @TODO
+# generate_manifests
+echo "Generating Manifests in tmp location."
+$hf --quiet template --skip-deps --output-dir="$k8sResourcesPath" >/dev/null
 
-    # for crd in "$jsonCRDS"; do
-    #     echo "DE~bug crd: $crd "
-    # done
+# generate canonical schemas
+targetYamlFiles="*.yaml"
+# schemas for otomi templates
+for file in $(find "$k8sResourcesPath" -name "$targetYamlFiles" -exec bash -c "ls {}" \;); do
+    process_crd $file
+done
+# schemas for chart crds
+for file in $(find charts/**/crds -name "$targetYamlFiles" -exec bash -c "ls {}" \;); do
+    process_crd $file
+done
+# create schema in canonical format for each extracted file
+echo "$(date --utc) Compiling all json-schemas from: $schemasBundleFile"
+for json in $(jq -s -r '.[] | .filename' $schemasBundleFile); do
+    jq "select(.filename==\"$json\")" $schemasBundleFile | jq '.schema' >"$schemaOutputPath/$version-standalone/$json"
+done
 
-}
-
-generate_schemas() {
-    local targetYamlFiles="*crd*.yaml"
-    for file in $(find "$k8sResourcesPath" -name "$targetYamlFiles" -exec bash -c 'ls "{}"' \;); do
-        filter_crds $file
-    done
-    generate_canonical_schemas
-
-}
-
-generate_manifests() {
-    echo "Generating Manifests in tmp location."
-    # using OUTPUT-DIR parameter because kubeval is not accepting multiple resources per file
-    $hf --quiet template --skip-deps --output-dir="$k8sResourcesPath" >/dev/null
-}
-
-validate_resources() {
-    local version=$1
-    echo "Validating Otomi Stack against Kubernetes Version: $version"
-    export KUBEVAL_SCHEMA_LOCATION="$destinationFile"
-    kubeval --force-color -d "$k8sResourcesPath" --exit-on-error --strict --kubernetes-version "$version"
-}
-
-version_allowed() {
-    for e in "${allowedVersions[@]}"; do [[ "$e" == "$1" ]] && exit 0; done
-    exit 1
-}
-
-validate_version() {
-    local version=$1
-    (
-        version_allowed "$version"
-    ) && echo "Version $version allowed" && exit 0 || echo "Version $version Not Found" && exit 1
-
-}
-
-version="v$(get_k8s_version).0"
-
-# version="v1.16.0"
-
-(validate_version $version) && versionValid=0
-if [[ $versionValid == 0 ]]; then
-    run_setup
-    generate_manifests
-    generate_schemas
-    validate_resources $version
-    exit 0
-else
-    exit 1
-fi
-
-# ERR - raw/templates/resources.yaml: Failed initializing schema /tmp/generated-crd-schemas/all.json/vv1.16.0-standalone-strict/configmap-v1.json: Reference /tmp/generated-crd-schemas/all.json/vv1.16.0-standalone-strict/configmap-v1.json must be canonical
-# Validation Failed
+# validate_resources()
+echo "Validating Otomi Stack against Kubernetes Version: $version"
+KUBEVAL_SCHEMA_LOCATION="file://./kubernetes-json-schema/master"
+kubeval --force-color -d "$k8sResourcesPath" --schema-location $KUBEVAL_SCHEMA_LOCATION --kubernetes-version $(echo $version | sed 's/v//') && exitcode=0
+# add --exit-on-error flag for fast exit
