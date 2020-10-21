b29d747164010070cadc75cdbf74bd83e66a3700
Martijn van Calker <martijn.vancalker@redkubes.com>
2020-10-21 11:09:04 +0200

    task: allow overridde variables via environment vars

--- bin/create-gke-cluster.sh
+++ bin/create-gke-cluster.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
-[ -z $ENV_DIR ] && echo "ENV_DIR must be set" && exit 1
-[ -z $CLUSTER ] && echo "CLUSTER must be set" && exit 1
+[[ -z $ENV_DIR ||  -z $CLUSTER ]] && echo "ENV_DIR and CLUSTER must be set" && exit 1
 
 readonly ENV_DIR
 readonly CLUSTER
 readonly METERING_SET=${METERING_SET:-otomi_metering}
 readonly VERBOSE=${VERBOSE:-0}
 
-readonly project=$(yq read $ENV_DIR/env/clusters.yaml clouds.google.projectId)
-readonly google_region=$(yq read $ENV_DIR/env/clusters.yaml clouds.google.clusters.$CLUSTER.region)
-readonly customer=$(yq read $ENV_DIR/env/settings.yaml customer.name)
-readonly k8s_version=$(yq read $ENV_DIR/env/clusters.yaml clouds.google.clusters.$CLUSTER.k8sVersion)
+readonly PROJECT_ID=${PROJECT_ID:-$(yq read $ENV_DIR/env/clusters.yaml clouds.google.projectId)}
+readonly GOOGLE_REGION=${GOOGLE_REGION:-$(yq read $ENV_DIR/env/clusters.yaml clouds.google.clusters.$CLUSTER.region)}
+readonly CUSTOMER=${CUSTOMER:-$(yq read $ENV_DIR/env/settings.yaml customer.name)}
+readonly K8S_VERSION=${K8S_VERSION:-$(yq read $ENV_DIR/env/clusters.yaml clouds.google.clusters.$CLUSTER.k8sVersion)}
 
 print_envs() {
   echo "ENV_DIR: $ENV_DIR"
   echo "CLUSTER: $CLUSTER"
-  echo "PROJECT: $project"
-  echo "GOOGLE_REGION: $google_region"
-  echo "K8S_VERSION: $k8s_version"
-  echo "CUSTOMER: $customer"
+  echo "PROJECT_ID: $PROJECT_ID"
+  echo "GOOGLE_REGION: $GOOGLE_REGION"
+  echo "K8S_VERSION: $K8S_VERSION"
+  echo "CUSTOMER: $CUSTOMER"
   echo "METERING_SET: $METERING_SET"
 }
 
@@ -28,9 +27,9 @@ print_envs() {
 
 # create the cluster
 gcloud container clusters create "otomi-gke-$CLUSTER" \
-  --project "$project" \
+  --project "$PROJECT_ID" \
   --addons HorizontalPodAutoscaling,HttpLoadBalancing \
-  --cluster-version $k8s_version \
+  --cluster-version $PROJECT_ID \
   --disk-size "100" \
   --disk-type "pd-standard" \
   --enable-autoprovisioning \
@@ -43,7 +42,7 @@ gcloud container clusters create "otomi-gke-$CLUSTER" \
   --enable-resource-consumption-metering \
   --enable-tpu \
   --image-type "COS" \
-  --labels customer=$customer \
+  --labels customer=$CUSTOMER \
   --machine-type "n1-standard-4" \
   --maintenance-window "01:00" \
   --max-cpu 8 \
@@ -53,17 +52,17 @@ gcloud container clusters create "otomi-gke-$CLUSTER" \
   --min-cpu 4 \
   --min-memory 8 \
   --min-nodes "1" \
-  --network "projects/$project/global/networks/default" \
+  --network "projects/$PROJECT_ID/global/networks/default" \
   --no-enable-basic-auth \
   --no-enable-stackdriver-kubernetes \
-  --node-labels customer=$customer \
+  --node-labels customer=$CUSTOMER \
   --num-nodes "1" \
-  --region "$google_region" \
+  --region "$GOOGLE_REGION" \
   --resource-usage-bigquery-dataset "$METERING_SET" \
   --scopes "https://www.googleapis.com/auth/cloud-platform" \
-  --subnetwork "projects/$project/regions/$google_region/subnetworks/default"
+  --subnetwork "projects/$PROJECT_ID/regions/$GOOGLE_REGION/subnetworks/default"
 # --enable-pod-security-policy
 
-gcloud container clusters get-credentials otomi-gke-$CLUSTER --region $google_region --project $project
+gcloud container clusters get-credentials otomi-gke-$CLUSTER --region $GOOGLE_REGION --project $PROJECT_ID
 
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
