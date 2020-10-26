f14f9588bd329e9907ee346e78fac7c7a881007e
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2020-10-26 17:22:28 +0100

    feat: marge all values before using them

--- bin/gen-drone.sh
+++ bin/gen-drone.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-set -e
+set -eu
 set -o pipefail
 
 . bin/common.sh
 . bin/colors.sh
 
-receiver=$(get_receiver)
+values=$(hf_values)
+receiver=$(echo "$values" | yq r - alerts.receiver)
 customer_name=$(customer_name)
-clustersPath="$ENV_DIR/env/clusters.yaml"
 tpl=$PWD/tpl/.drone.tpl.$receiver.yml
 otomi_image_tag=$(otomi_image_tag)
 
@@ -16,10 +16,9 @@ if [ "$receiver" == "slack" ]; then
 else
   key="lowPrio"
 fi
-
-# Note: the .secrets.yaml exists only if .secrets.yaml.enc has been decrypted
-webhook=$(get_receiver $key)
-clouds=($(yq r -j $clustersPath clouds | jq -r '.|keys[]'))
+set -x
+webhook=$(echo "$values" | yq r - "alerts.$receiver.$key")
+clouds=($(yq r -j $clustersFile clouds | jq -r '.|keys[]'))
 
 function template_drone_config() {
 
@@ -29,17 +28,18 @@ function template_drone_config() {
   local cluster=$4
   local otomi_image_tag=$5
   printf "${COLOR_LIGHT_PURPLE}Creating $targetPath ${COLOR_NC}\n"
+
   cat $templatePath | sed -e "s/__CLOUD/${cloud}/g" -e "s/__CLUSTER/${cluster}/g" \
     -e "s/__IMAGE_TAG/${otomi_image_tag}/g" -e "s|__WEBHOOK|${webhook}|g" \
     -e "s/__CUSTOMER/${customer_name}/g" \
     >$targetPath
 }
 
-for cloud in ${clouds[@]}; do
-  clusters=($(yq r -j $clustersPath clouds.${cloud}.clusters | jq -r '.|keys[]'))
-  for cluster in ${clusters[@]}; do
+for cloud in "${clouds[@]}"; do
+  clusters=($(yq r -j $clustersFile clouds.${cloud}.clusters | jq -r '.|keys[]'))
+  for cluster in "${clusters[@]}"; do
     targetPath="$ENV_DIR/env/clouds/${cloud}/${cluster}/.drone.yml"
-    otomiVersion="$(yq r $clustersPath clouds.${cloud}.clusters.${cluster}.otomiVersion)"
+    otomiVersion="$(yq r $clustersFile clouds.${cloud}.clusters.${cluster}.otomiVersion)"
     template_drone_config $targetPath $tpl $cloud $cluster $otomiVersion
   done
 done
