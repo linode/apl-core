de9ac20526bd5ff71cb8615aab39d17c70815daa
Maurice Faber <maurice.faber@redkubes.com>
2020-10-28 00:52:11 +0100

    improvement: added stale bot, pinned tasks to new public repo

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'improvement', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
+  },
+}
