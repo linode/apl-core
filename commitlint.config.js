aacd96c942c0ef7f9c93b566d1d365f31da15ff2
Maurice Faber <maurice.faber@redkubes.com>
2020-10-28 10:34:21 +0100

    chore(commitizen): reverted to config-conventional only to simplify

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'improvement', 'perf', 'refactor', 'revert', 'style', 'test'],
-    ],
-  },
 }
