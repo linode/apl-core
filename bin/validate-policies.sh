bbe82b739a9d98796285b990bb09e2f68bc4fb87
Alin Spinu <spinualin@gmail.com>
2020-11-03 13:16:42 +0200

    fix: mv policies top level

--- bin/validate-policies.sh
+++ bin/validate-policies.sh
@@ -6,7 +6,7 @@ EXIT_FAST=${EXIT_FAST:-"1"}
 [[ $EXIT_FAST == "1" ]] && set -e
 
 k8sResourcesPath="/tmp/otomi/conftest-fixtures"
-policiesPath="values/gatekeeper-operator/policies"
+policiesPath="policies"
 exitcode=1
 
 . bin/common.sh
@@ -34,7 +34,7 @@ validate_policies() {
     # validate_resources
     echo "Run Policy validation for ${CLOUD}-${CLUSTER} template resources"
     {
-        conftest test --fail-on-warn --all-namespaces -d "$policiesPath/lib/constraints.yaml" -p "$policiesPath/lib/helpers.rego" -p $policiesPath $k8sResourcesPath
+        conftest test --fail-on-warn --all-namespaces -d "$policiesPath/constraints.yaml" -p "$policiesPath/lib/helpers.rego" -p $policiesPath $k8sResourcesPath
     } && exitcode=0
 
 }
