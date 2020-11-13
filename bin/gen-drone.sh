ba297795212ad0b6961948017721d6807a65384c
Maurice Faber <maurice.faber@redkubes.com>
2020-11-14 00:04:48 +0100

    fix: drone gen [ci skip]

--- bin/gen-drone.sh
+++ bin/gen-drone.sh
@@ -2,7 +2,6 @@
 set -eu
 set -o pipefail
 
-
 ENV_DIR=${ENV_DIR:-./env}
 . $ENV_DIR/.secrets
 
@@ -33,7 +32,7 @@ function template_drone_config() {
   cat $templatePath | sed -e "s/__CLOUD/${CLOUD}/g" -e "s/__CLUSTER/${CLUSTER}/g" \
     -e "s/__IMAGE_TAG/${otomi_image_tag}/g" -e "s|__WEBHOOK|${webhook}|g" \
     -e "s/__CUSTOMER/${customer_name}/g" \
-    > $target
+    >$target
 }
 
 for_each_cluster template_drone_config
