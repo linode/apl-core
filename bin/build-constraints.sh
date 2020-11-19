76a404bcd9e356145d456a7dc0e11476ee387a7f
Alin Spinu <spinualin@gmail.com>
2020-11-19 19:29:04 +0200

    feat: add parameters values schema (wip) [ci skip]

--- bin/build-constraints.sh
+++ bin/build-constraints.sh
@@ -16,18 +16,18 @@ function build() {
 
 function decorate() {
   echo "Decorating template/constraints files with properties."
-  local parseConstraintsExpression='.constraints as $constraints |  $constraints | keys[] | {(.): $constraints[.]}'
-  for constraint in $(hf_values | yq r -j - 'charts.gatekeeper' | jq --raw-output -S -c "$parseConstraintsExpression"); do
+  local mapConstraintsExpr='.constraints as $constraints |  $constraints | keys[] | {(.): $constraints[.]}'
+  for constraint in $(hf_values | yq r -j - 'charts.gatekeeper' | jq --raw-output -S -c "$mapConstraintsExpr"); do
     local key=$(echo $constraint | jq --raw-output '. | keys[0]')
-    # decorate constraints
+    # decorate constraints with parameters
     local constraintsFile=$(ls $outputPath/constraint_* | grep -i "$key.yaml")
     local parameters=$(echo $constraint | jq --raw-output -c "{"spec":{"parameters": {"${key}"} }}")
     local constraints=$(yq r -P -j $constraintsFile | jq --raw-output -c '.')
     jq -n --argjson constraints $constraints --argjson parameters $parameters '$constraints * $parameters | .' | yq r -P - >$constraintsFile
-    # decorate constraint templates
+    # decorate constraint templates with openAPI schema properties
     local mapPropertiesExpr='. as $properties | {"spec":{"crd":{"spec":{"validation": {"openAPIV3Schema": $properties }}}}} | .'
-    local policyPropertiesPath="properties.charts.properties.gatekeeper.properties.constraints.properties[${key}]"
-    local properties=$(yq -j r values-schema.yaml $policyPropertiesPath | jq -c --raw-output "$mapPropertiesExpr")
+    local policyJSONPath="properties.charts.properties.gatekeeper.properties.constraints.properties[${key}]"
+    local properties=$(yq -j r values-schema.yaml $policyJSONPath | jq -c --raw-output "$mapPropertiesExpr")
     local ctemplatesFile=$(ls $outputPath/template_* | grep -i "$key.yaml")
     local template=$(yq r -P -j $ctemplatesFile | jq --raw-output -c '.')
     jq -n --argjson template "$template" --argjson properties "$properties" '$template * $properties | .' | yq r -P - >$ctemplatesFile
