282e0012d1a68ed7f6e939664286162c164f731f
Alin Spinu <spinualin@gmail.com>
2020-11-19 11:26:27 +0200

    fix: build constraints in tmp folder

--- bin/build-constraints.sh
+++ bin/build-constraints.sh
@@ -6,12 +6,12 @@ runFromHook=$1
 
 . ./bin/common.sh
 
-gatekeeperArtifactsPath="./values/gatekeeper/constraints"
+outputPath="/tmp/otomi/constraints"
 function build() {
   echo "Building constraints artifacts from policies."
   local policiesPath="./policies"
-  rm -f "$gatekeeperArtifactsPath/*"
-  konstraint create $policiesPath -o $gatekeeperArtifactsPath
+  rm -f "$outputPath/*"
+  konstraint create $policiesPath -o $outputPath
 }
 
 function decorate() {
@@ -20,7 +20,7 @@ function decorate() {
   for constraint in $(hf_values | yq r -j - 'charts.gatekeeper' | jq --raw-output -S -c "$parseConstraintsExpression"); do
     local key=$(echo $constraint | jq --raw-output '. | keys[0]')
     # decorate constraints
-    local constraintsFile=$(ls $gatekeeperArtifactsPath/constraint_* | grep -i "$key.yaml")
+    local constraintsFile=$(ls $outputPath/constraint_* | grep -i "$key.yaml")
     local parameters=$(echo $constraint | jq --raw-output -c "{"spec":{"parameters": {"${key}"} }}")
     local constraints=$(yq r -P -j $constraintsFile | jq --raw-output -c '.')
     jq -n --argjson constraints $constraints --argjson parameters $parameters '$constraints * $parameters | .' | yq r -P - >$constraintsFile
@@ -28,7 +28,7 @@ function decorate() {
     local mapPropertiesExpr='. as $properties | {"spec":{"crd":{"spec":{"validation": {"openAPIV3Schema": $properties }}}}} | .'
     local policyPropertiesPath="properties.charts.properties.gatekeeper.properties.constraints.properties[${key}]"
     local properties=$(yq -j r values-schema.yaml $policyPropertiesPath | jq -c --raw-output "$mapPropertiesExpr")
-    local ctemplatesFile=$(ls $gatekeeperArtifactsPath/template_* | grep -i "$key.yaml")
+    local ctemplatesFile=$(ls $outputPath/template_* | grep -i "$key.yaml")
     local template=$(yq r -P -j $ctemplatesFile | jq --raw-output -c '.')
     jq -n --argjson template "$template" --argjson properties "$properties" '$template * $properties | .' | yq r -P - >$ctemplatesFile
   done
