a009b38eb282600905e8f31472ef33e796c7f3e4
Marc Went <marc.went+git@redkubes.com>
2021-01-07 21:33:17 +0100

    refactor: move has_docker checks to common.sh and use it to check docker and run yq in container
    Created a shadow function yq, instead of dyq. Move a couple of variables from otomi to common.sh
    that are necessary in both common & otomi. Also added `function` keyword for every function that
    misses it, to bring it in line to the same style as otomi & bootstrap. Also fix bug in otomi.sh
    `check_docker_running` where the exit code was used, now it uses `has_docker` that uses a similar
    check, but is now in common.sh
    
    fix #271

--- bin/common.sh
+++ bin/common.sh
@@ -6,34 +6,40 @@ readonly clustersFile="$ENV_DIR/env/clusters.yaml"
 readonly helmfileOutputHide="(^\W+$|skipping|basePath=|Decrypting)"
 readonly helmfileOutputHideTpl="(^[\W^-]+$|skipping|basePath=|Decrypting)"
 readonly replacePathsPattern="s@../env@${ENV_DIR}@g"
-readonly yqDockerImage="mikefarah/yq:3"
+readonly otomiToolsImage="otomi/tools:1.4.10"
 
-function dyq() {
-  local yqcommand=$@
-  if [ -x "$(command -v yq)" ] && [[ "$(yq --version 2>&1 | sed 's/yq version //g')" == "3."* ]] ; then
-    yq ${yqcommand}
-  elif [ -x "$(command -v docker)" ]; then
-    docker run --rm -v ${ENV_DIR}:${ENV_DIR} ${yqDockerImage} yq ${yqcommand}
+has_docker=$( docker --version &>/dev/null && echo "1" || echo "0")
+dind=0
+cmd_image=''
+
+function yq() {
+  local yq_args=$@
+  if [[ $has_docker -eq 1 && ("${IN_DOCKER}" != "1" || $dind -eq 1) ]]; then
+    docker run --rm \
+      -v ${ENV_DIR}:${ENV_DIR} \
+      -e CLOUD="$CLOUD" \
+      -e IN_DOCKER="1" \
+      -e CLUSTER="$CLUSTER" \
+      ${otomiToolsImage} yq ${yq_args}
   else
-    echo "ERROR: Either docker needs to be installed, or yq@3 needs to be installed." >&2
-    exit 1
+    command yq ${yq_args}
   fi
 }
 
-get_k8s_version() {
-  dyq r $clustersFile "clouds.$CLOUD.clusters.$CLUSTER.k8sVersion"
+function get_k8s_version() {
+  yq r $clustersFile "clouds.$CLOUD.clusters.$CLUSTER.k8sVersion"
 }
 
-otomi_image_tag() {
-  local otomiVersion=$(dyq r $clustersFile "clouds.$CLOUD.clusters.$CLUSTER.otomiVersion")
+function otomi_image_tag() {
+  local otomiVersion=$([ -n "${CLOUD+x}${CLUSTER+x}" ] && yq r $clustersFile "clouds.$CLOUD.clusters.$CLUSTER.otomiVersion")
   [[ -n $otomiVersion ]] && echo $otomiVersion || echo 'latest'
 }
 
-customer_name() {
-  dyq r $otomiSettings "customer.name"
+function customer_name() {
+  yq r $otomiSettings "customer.name"
 }
 
-check_sops_file() {
+function check_sops_file() {
   [[ ! -f "$ENV_DIR/.sops.yaml" ]] && (
     echo "Error: The $ENV_DIR/.sops.yaml does not exists" >&2
     exit 1
@@ -41,33 +47,34 @@ check_sops_file() {
   return 0
 }
 
-hf() {
+function hf() {
   helmfile --quiet -e $CLOUD-$CLUSTER $@
 }
 
-hf_values() {
+function hf_values() {
   [ "${VERBOSE-'false'}" = 'false' ] && quiet='--quiet'
   helmfile ${quiet-} -e "$CLOUD-$CLUSTER" -f helmfile.tpl/helmfile-dump.yaml build | grep -Ev $helmfileOutputHide | sed -e $replacePathsPattern |
-    dyq read -P - 'releases[0].values[0]'
+    yq read -P - 'releases[0].values[0]'
 }
 
-prepare_crypt() {
+function prepare_crypt() {
   [[ -z "$GCLOUD_SERVICE_KEY" ]] && echo "Error: The GCLOUD_SERVICE_KEY environment variable is not set" >&2 && exit 2
   GOOGLE_APPLICATION_CREDENTIALS="/tmp/key.json"
   echo $GCLOUD_SERVICE_KEY >$GOOGLE_APPLICATION_CREDENTIALS
   export GOOGLE_APPLICATION_CREDENTIALS
 }
 
-for_each_cluster() {
+function for_each_cluster() {
   # Perform a command from argument for each cluster
   executable=$1
   [[ -z "$executable" ]] && echo "ERROR: the positional argument is not set" >&2
   local clustersPath="$ENV_DIR/env/clusters.yaml"
-  clouds=$(dyq r -j $clustersPath clouds | jq -rc '.|keys[]')
+  clouds=$(yq r -j $clustersPath clouds | jq -rc '.|keys[]')
   for cloud in $clouds; do
-    clusters=($(dyq r -j $clustersPath clouds.${cloud}.clusters | jq -rc '. | keys[]'))
+    clusters=($(yq r -j $clustersPath clouds.${cloud}.clusters | jq -rc '. | keys[]'))
     for cluster in "${clusters[@]}"; do
       CLOUD=$cloud CLUSTER=$cluster $executable
     done
   done
 }
+
