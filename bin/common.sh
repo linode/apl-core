57913c85407de362e10e839f306c9a7d969b5b66
Marc Went <marc.went+git@redkubes.com>
2021-01-15 13:32:01 +0100

    fix: add a print_error function and create a "generic" _shadow function for yq & jq
    Using the _shadow function it's easier to create shadow functions that run the command either in
    docker or native, or error when neither is available. The print_error function prepends "Error: " to
    the message and redirects to SDTERR insted of STDOUT
    
    #270 #271

--- bin/common.sh
+++ bin/common.sh
@@ -12,18 +12,37 @@ has_docker=$( docker --version &>/dev/null && echo "1" || echo "0")
 dind=0
 cmd_image=''
 
-function yq() {
-  local yq_args=$@
+function print_error() {
+    echo "Error: " $@ >&2
+}
+
+function _shadow() {
+  local cmd=$1
+  shift
+  local args=$@
   if [[ $has_docker -eq 1 && ("${IN_DOCKER}" != "1" || $dind -eq 1) ]]; then
     docker run --rm \
       -v ${ENV_DIR}:${ENV_DIR} \
       -e CLOUD="$CLOUD" \
       -e IN_DOCKER="1" \
       -e CLUSTER="$CLUSTER" \
-      ${otomiToolsImage} yq ${yq_args}
+      ${otomiToolsImage} ${cmd} ${args}
+  elif command -v ${cmd} &>/dev/null; then
+    command ${cmd} ${args}
   else
-    command yq ${yq_args}
+    print_error "Docker is not available and ${cmd} is not installed locally"
+    exit 1
   fi
+}
+
+function yq() {
+  local yq_args=$@
+  _shadow yq $yq_args
+}
+
+function jq() {
+  local jq_args=$@
+  _shadow jq $jq_args
 }
 
 function get_k8s_version() {
@@ -41,7 +60,7 @@ function customer_name() {
 
 function check_sops_file() {
   [[ ! -f "$ENV_DIR/.sops.yaml" ]] && (
-    echo "Error: The $ENV_DIR/.sops.yaml does not exists" >&2
+    print_error "The $ENV_DIR/.sops.yaml does not exists"
     exit 1
   )
   return 0
@@ -58,7 +77,7 @@ function hf_values() {
 }
 
 function prepare_crypt() {
-  [[ -z "$GCLOUD_SERVICE_KEY" ]] && echo "Error: The GCLOUD_SERVICE_KEY environment variable is not set" >&2 && exit 2
+  [[ -z "$GCLOUD_SERVICE_KEY" ]] && print_error "The GCLOUD_SERVICE_KEY environment variable is not set" && exit 2
   GOOGLE_APPLICATION_CREDENTIALS="/tmp/key.json"
   echo $GCLOUD_SERVICE_KEY >$GOOGLE_APPLICATION_CREDENTIALS
   export GOOGLE_APPLICATION_CREDENTIALS
@@ -67,7 +86,7 @@ function prepare_crypt() {
 function for_each_cluster() {
   # Perform a command from argument for each cluster
   executable=$1
-  [[ -z "$executable" ]] && echo "ERROR: the positional argument is not set" >&2
+  [[ -z "$executable" ]] && print_error "The positional argument is not set"
   local clustersPath="$ENV_DIR/env/clusters.yaml"
   clouds=$(yq r -j $clustersPath clouds | jq -rc '.|keys[]')
   for cloud in $clouds; do
