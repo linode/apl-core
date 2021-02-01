6699ddb811cd740505ebec40f2508eed8aa1c311
Maurice Faber <maurice.faber@redkubes.com>
2021-02-01 19:13:26 +0100

    fix: crypt (#304)

--- bin/common.sh
+++ bin/common.sh
@@ -90,7 +90,6 @@ function hf_values() {
 }
 
 function prepare_crypt() {
-  [ -n "$GOOGLE_APPLICATION_CREDENTIALS" ] && return 0
   [ -z "$GCLOUD_SERVICE_KEY" ] && err "The GCLOUD_SERVICE_KEY environment variable is not set" && exit 2
   GOOGLE_APPLICATION_CREDENTIALS="/tmp/key.json"
   echo $GCLOUD_SERVICE_KEY >$GOOGLE_APPLICATION_CREDENTIALS
