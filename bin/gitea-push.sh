c8777cb186ce2ec2f618aeb1689c62e9ba88f158
Marc Went <marc.went+git@redkubes.com>
2021-03-15 10:31:59 +0100

    fix: implement review from @j-zimnowoda
    Implement gitea_url/cluster_domain var and add more documentation for why security context can't be
    used (yet)
    
    #293

--- bin/gitea-push.sh
+++ bin/gitea-push.sh
@@ -6,9 +6,10 @@ set -eo pipefail
 prepare_crypt
 readonly values=$(hf_values)
 readonly gitea_enabled=$(echo "$values" | yq r - 'charts.gitea.enabled')
-[ "$gitea_enabled" != "true" ] && err "Gitea is disabled" && exit 1
+[ "$gitea_enabled" != "true" ] && err "Gitea is disabled" && exit 0
 
-readonly gitea_url=$(echo "$values" | yq r - 'cluster.domain')
+readonly cluster_domain=$(echo "$values" | yq r - 'cluster.domain')
+readonly gitea_url="gitea.$cluster_domain"
 readonly gitea_password=$(echo "$values" | yq r - 'charts.gitea.admin.password')
 readonly gitea_user='otomi'
 readonly gitea_repo='values'
@@ -21,11 +22,11 @@ if [ ! -d .git ]; then
   git init
 fi
 # Check if gitea is origin, otherwise stop
-if [ $(git config remote.$remote_name.url) ] && [ "gitea.$gitea_url" != $(git config remote.$remote_name.url | cut -d@ -f2 | cut -d/ -f1 | cut -d: -f1) ]; then
+if [ $(git config remote.$remote_name.url) ] && [ $gitea_url != $(git config remote.$remote_name.url | cut -d@ -f2 | cut -d/ -f1 | cut -d: -f1) ]; then
   err "Another origin already exists, not using gitea"
   exit 1
 else
-  git remote add $remote_name "https://$gitea_user:$gitea_password@gitea.$gitea_url/$gitea_user/$gitea_repo.git"
+  git remote add $remote_name "https://$gitea_user:$gitea_password@$gitea_url/$gitea_user/$gitea_repo.git"
   echo "Added gitea as a remote origin"
 fi
 
@@ -36,7 +37,7 @@ readonly commit_count=$(git rev-list --count --remotes=$remote_name)
 
 if [ "$commit_count" -eq "0" ]; then
     git config user.name "Otomi"
-    git config user.email "otomi@$gitea_url"
+    git config user.email "otomi@$cluster_domain"
     git add -A
 
     git commit --no-verify -m "Initial commit of otomi-values"
