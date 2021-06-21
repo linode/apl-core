4cf15730cef92032e918a536b310d1841c627aa6
Maurice Faber <maurice.faber@redkubes.com>
2021-06-21 21:54:11 +0200

    fix: harbor perms, destroy script

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -10,8 +10,11 @@ hf destroy
 hf -f helmfile.tpl/helmfile-init.yaml template | kubectl delete -f -
 
 # delete all crds installed by us
-crds="(appgw.ingress.k8s.io|cert-manager.io|externalsecrets.kubernetes-client.io|gatekeeper.sh|istio.io|knative.dev|kubeapps.com|monitoring.coreos.com|monitoring.kiali.io|operators.coreos.com|vault.banzaicloud.com)"
-k get crd | grep -E $crds | awk '{ print $1 }' | awk '(NR>1)' | xargs kubectl delete crd
+crds="(appgw.ingress.k8s.io|cert-manager.io|externalsecrets.kubernetes-client.io|gatekeeper.sh|istio.io|jaegertracing.io|kiali.io|knative.dev|kubeapps.com|coreos.com|vault.banzaicloud.com)"
+k get crd --no-headers -o custom-columns=":metadata.name" | grep -E $crds | xargs kubectl delete crd
 
 # and to finally remove all hanging namespaces which are stuck on:
 k delete apiservices.apiregistration.k8s.io v1.packages.operators.coreos.com
+
+echo "The following PVCs remain in the cluster:"
+k get pvc -A
