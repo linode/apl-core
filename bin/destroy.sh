4688cc532ea294ed785c028216bd4d1a22845bcb
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2021-06-21 09:59:15 +0200

    feat: add gatekeepr to crds to destroy

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -10,8 +10,8 @@ hf destroy
 # hf -f helmfile.tpl/helmfile-init.yaml template | kubectl delete -f -
 
 # delete all crds installed by us
-crds="(appgw.ingress.k8s.io|cert-manager.io|externalsecrets.kubernetes-client.io|istio.io|kubeapps.com|monitoring.coreos.com|monitoring.kiali.io|operators.coreos.com|vault.banzaicloud.com)"
-k get crd | grep -E $crds | awk '{ print $1 }' | awk '(NR>1)' | xargs kubectl delete crd
+crds="(appgw.ingress.k8s.io|cert-manager.io|externalsecrets.kubernetes-client.io|gatekeeper.sh|istio.io|kubeapps.com|monitoring.coreos.com|monitoring.kiali.io|operators.coreos.com|vault.banzaicloud.com)"
+k get crd --no-headers -o custom-columns=":metadata.name" | grep -E $crds | xargs kubectl delete crd
 
 # and to finally remove all hanging namespaces which are stuck on:
 k delete apiservices.apiregistration.k8s.io v1.packages.operators.coreos.com
