623a34017161b8b6c2d81dbc4d81f09c6e77029a
Mojtaba <sm.imani@gmail.com>
2021-06-28 11:40:20 +0200

    fix: removing redundant function

--- bin/common.sh
+++ bin/common.sh
@@ -234,19 +234,3 @@ function hf_template() {
     hf template --skip-deps $SKIP_CLEANUP
   fi
 }
-
-parse_yaml() {
-  local prefix=$2
-  local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @ | tr @ '\034')
-  echo $1 | sed -e "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-    -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
-    awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-        vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-        printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-    }'
-}
