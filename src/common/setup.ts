d96394696eb8cd424dd455b2769f289e1942104a
Marc Went <marc.went+git@redkubes.com>
2021-06-30 10:04:23 +0200

    fix: 🐛 Fix Maurice's suggestions

--- src/common/setup.ts
+++ src/common/setup.ts
@@ -15,91 +15,89 @@ let otomiImageTag: string
 let otomiCustomerName: string
 let otomiK8sVersion: string
 
-const noExport = {
-  /**
-   * Check whether the environment matches the configuration for the Kubernetes Context
-   * @param debug
-   * @returns
-   */
-  checkKubeContext: async (debug: OtomiDebugger): Promise<void> => {
-    if (ENV.isCI) return
-    debug.verbose('Validating kube context')
+/**
+ * Check whether the environment matches the configuration for the Kubernetes Context
+ * @param debug
+ * @returns
+ */
+const checkKubeContext = async (debug: OtomiDebugger): Promise<void> => {
+  if (ENV.isCI) return
+  debug.verbose('Validating kube context')
 
-    const envPath = `${ENV.DIR}/env/.env`
-    try {
-      await source(envPath)
-    } catch (error) {
-      debug.exit(1, error)
+  const envPath = `${ENV.DIR}/env/.env`
+  try {
+    await source(envPath)
+  } catch (error) {
+    debug.exit(1, error)
+  }
+
+  if (!('K8S_CONTEXT' in process.env)) debug.exit(1, `K8S_CONTEXT is not defined in '${envPath}'`)
+  debug.verbose(`Using kube context: ${process.env.K8S_CONTEXT}`)
+
+  // TODO: Consider using the kubernetes-client: https://github.com/kubernetes-client/javascript
+  const runningContext = (await nothrow($`kubectl config current-context`)).stdout.trim()
+  if (process.env.K8S_CONTEXT !== runningContext) {
+    let fixContext = false
+    if (!(parser.argv as BasicArguments).setContext) {
+      fixContext = await askYesNo(
+        `Warning: Your current kubernetes context does not match cluster context: ${process.env.K8S_CONTEXT}. Would you like to switch kube context to cluster first?`,
+        { defaultYes: true },
+      )
     }
-
-    if (!('K8S_CONTEXT' in process.env)) debug.exit(1, `K8S_CONTEXT is not defined in '${envPath}'`)
-    debug.verbose(`Using kube context: ${process.env.K8S_CONTEXT}`)
-
-    // TODO: Consider using the kubernetes-client: https://github.com/kubernetes-client/javascript
-    const runningContext = (await nothrow($`kubectl config current-context`)).stdout.trim()
-    if (process.env.K8S_CONTEXT !== runningContext) {
-      let fixContext = false
-      if (!(parser.argv as BasicArguments).setContext) {
-        fixContext = await askYesNo(
-          `Warning: Your current kubernetes context does not match cluster context: ${process.env.K8S_CONTEXT}. Would you like to switch kube context to cluster first?`,
-          { defaultYes: true },
-        )
+    if (fixContext || (parser.argv as BasicArguments).setContext) {
+      await $`kubectl config use ${process.env.K8S_CONTEXT}`
+    }
+  }
+}
+/**
+ * Check the ENV_DIR parameter and whether or not the folder is populated
+ * @param debug
+ * @returns
+ */
+const checkENVdir = (debug: OtomiDebugger): boolean => {
+  if (dirname.includes('otomi-core') && !('ENV_DIR' in process.env)) {
+    debug.exit(1, 'The ENV_DIR environment variable is not set')
+  }
+  debug.debug(ENV.DIR)
+  return readdirSync(ENV.DIR).length > 0
+}
+/**
+ * Find (recursively) the object mapped under the `id` within the `obj`
+ * @param obj
+ * @param id
+ * @returns
+ */
+const getNestedObjectValue = (obj: any, id: string): any | undefined => {
+  const getObject = (theObject: any, objectId: string): any => {
+    let result = null
+    if (Array.isArray(theObject)) {
+      for (let i = 0; i < theObject.length; i++) {
+        result = getObject(theObject[i], objectId)
+        if (result) {
+          break
+        }
       }
-      if (fixContext || (parser.argv as BasicArguments).setContext) {
-        await $`kubectl config use ${process.env.K8S_CONTEXT}`
-      }
-    }
-  },
-  /**
-   * Check the ENV_DIR parameter and whether or not the folder is populated
-   * @param debug
-   * @returns
-   */
-  checkENVdir: (debug: OtomiDebugger): boolean => {
-    if (dirname.includes('otomi-core') && !('ENV_DIR' in process.env)) {
-      debug.exit(1, 'The ENV_DIR environment variable is not set')
-    }
-    debug.debug(ENV.DIR)
-    return readdirSync(ENV.DIR).length > 0
-  },
-  /**
-   * Find (recursively) the object mapped under the `id` within the `obj`
-   * @param obj
-   * @param id
-   * @returns
-   */
-  getNestedObjectValue: (obj: any, id: string): any | undefined => {
-    const getObject = (theObject: any, objectId: string): any => {
-      let result = null
-      if (Array.isArray(theObject)) {
-        for (let i = 0; i < theObject.length; i++) {
-          result = getObject(theObject[i], objectId)
+    } else {
+      /* eslint-disable no-restricted-syntax */
+      for (const [key, value] of Object.entries(theObject)) {
+        if (key === objectId) {
+          return theObject
+        }
+        if (value instanceof Object || Array.isArray(value)) {
+          result = getObject(value, objectId)
           if (result) {
             break
           }
         }
-      } else {
-        /* eslint-disable no-restricted-syntax */
-        for (const [key, value] of Object.entries(theObject)) {
-          if (key === objectId) {
-            return theObject
-          }
-          if (value instanceof Object || Array.isArray(value)) {
-            result = getObject(value, objectId)
-            if (result) {
-              break
-            }
-          }
-        }
       }
-      /* eslint-enable no-restricted-syntax */
-
-      return result
     }
-    const myobj: any = getObject(obj, id)
-    if (!myobj) return undefined
-    return myobj[id]
-  },
+    /* eslint-enable no-restricted-syntax */
+
+    return result
+  }
+  const myobj: any = getObject(obj, id)
+  if (!myobj) return undefined
+  return myobj[id]
 }
 
 export type PrepareEnvironmentOptions = {
@@ -117,7 +115,7 @@ export const otomi = {
    */
   getK8sVersion: async (): Promise<string> => {
     if (otomiK8sVersion) return otomiK8sVersion
-    otomiK8sVersion = noExport.getNestedObjectValue(await values(), 'cluster').k8sVersion
+    otomiK8sVersion = getNestedObjectValue(await values(), 'cluster').k8sVersion
     return otomiK8sVersion
   },
   /**
@@ -150,10 +148,10 @@ export const otomi = {
   prepareEnvironment: async (debugPar: OtomiDebugger, options?: PrepareEnvironmentOptions): Promise<void> => {
     const debug = debugPar.extend('prep environment')
     debug.verbose('Checking environment')
-    if (!options?.skipEnvDirCheck && noExport.checkENVdir(debug)) {
-      if (!('CI' in process.env) && !options?.skipEvaluateSecrets) await evaluateSecrets(debug)
-      if (!('CI' in process.env) && !options?.skipKubeContextCheck) await noExport.checkKubeContext(debug)
-      if (!('CI' in process.env) && !options?.skipDecrypt) await decrypt(debug)
+    if (!options?.skipEnvDirCheck && checkENVdir(debug)) {
+      if (!ENV.isCI && !options?.skipEvaluateSecrets) await evaluateSecrets(debug)
+      if (!ENV.isCI && !options?.skipKubeContextCheck) await checkKubeContext(debug)
+      if (!ENV.isCI && !options?.skipDecrypt) await decrypt(debug)
     }
   },
   /**
