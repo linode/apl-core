3c2a85bbfda5044605cd8975fae2e8dace3346b9
Maurice Faber <maurice.faber@redkubes.com>
2021-06-30 21:42:19 +0200

    fix: pre-commit

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -5,5 +5,4 @@ set -o pipefail
 ENV_DIR=${ENV_DIR:-./env}
 
 changes=$(git -C $ENV_DIR diff env/settings.yaml | grep '^+    version:.*$')
-[ "$changes" = "" ] && echo "No otomi version changes detected. Skipping." && exit
-bin/gen-drone.sh
+[ "$changes" != "" ] && bin/gen-drone.sh
