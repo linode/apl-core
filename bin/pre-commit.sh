b5c90f991a9d0609bd5b3e8cdf1d2ae03d9f6d52
Maurice Faber <maurice.faber@redkubes.com>
2021-06-30 18:07:08 +0200

    fix: gen-drone dependent on otomi version change

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 set -o pipefail
 
 ENV_DIR=${ENV_DIR:-./env}
 
-match="cluster.yaml"
-if git -C $ENV_DIR diff --name-only | grep $match >/dev/null; then
-  bin/gen-drone.sh
-fi
+changes=$(git -C $ENV_DIR diff env/settings.yaml | grep '^+    version:.*$')
+[ "$changes" = "" ] && echo "No otomi version changes detected. Skipping." && exit
+bin/gen-drone.sh
