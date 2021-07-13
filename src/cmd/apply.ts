5e60b445ffd91f24386c07e358314032faccb41b
Marc Went <marc.went+git@redkubes.com>
2021-07-13 13:31:08 +0200

    refactor: 💡 remove gen tls certs. It's become separate script
    It is no longer part of deploy process, and is a script that runs on
    it's own. Doesn't require docker or anything. So keeping it as shell
    script

--- src/cmd/apply.ts
+++ src/cmd/apply.ts
@@ -1,10 +1,10 @@
-import { existsSync, mkdirSync, rmdirSync, writeFileSync } from 'fs'
+import { mkdirSync, rmdirSync, writeFileSync } from 'fs'
 import { Argv, CommandModule } from 'yargs'
 import { $ } from 'zx'
 import { OtomiDebugger, terminal } from '../common/debug'
 import { giteaPush } from '../common/gitea-push'
 import { Arguments as HelmArgs, helmOptions } from '../common/helm-opts'
-import { hf, hfStream, hfValues } from '../common/hf'
+import { hf, hfStream } from '../common/hf'
 import { ENV, LOG_LEVEL_STRING } from '../common/no-deps'
 import { cleanupHandler, otomi, PrepareEnvironmentOptions } from '../common/setup'
 import { ProcessOutputTrimmed } from '../common/zx-enhance'
@@ -14,7 +14,6 @@ import { Arguments as DroneArgs, genDrone } from './gen-drone'
 const fileName = 'apply'
 const dir = '/tmp/otomi/'
 const templateFile = `${dir}deploy-template.yaml`
-const ssl = `${dir}ssl`
 let debug: OtomiDebugger
 
 interface Arguments extends HelmArgs, DroneArgs {}
@@ -35,32 +34,7 @@ const setup = async (argv: Arguments, options?: PrepareEnvironmentOptions): Prom
   mkdirSync(dir, { recursive: true })
 }
 
-const genDemoMtlsCertSecret = async () => {
-  const hfVals = await hfValues()
-  const root = hfVals.cluster.domainSuffix
-  const dom = `tlspass.${root}`
-  if (existsSync(`${ssl}/${root}.crt`)) return
-  mkdirSync(ssl, { recursive: true })
-
-  // for demonstration of mtls passthrough
-  // see https://istio.io/latest/docs/tasks/traffic-management/ingress/ingress-sni-passthrough/
-  await $`openssl req -out ${ssl}/${dom}.csr -newkey rsa:2048 -nodes -keyout ${ssl}/${dom}.key -subj '/CN=${dom}/O=some organization' -sha256`
-  await $`openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=example Inc./CN=example.com' -keyout ${ssl}/${root}.key -out ${ssl}/${root}.crt -sha256`
-  await $`openssl x509 -req -days 365 -CA ${ssl}/${root}.crt -CAkey ${ssl}/${root}.key -set_serial 0 -in ${ssl}/${dom}.csr -out ${ssl}/${dom}.crt -sha256`
-
-  // try to create the secret if it does not yet exist
-  try {
-    await $`kubectl get ns team-demo`
-    await $`kubectl -n team-demo get secret nginx-server-certs`
-  } catch (error) {
-    await $`kubectl -n team-demo create secret tls nginx-server-certs --key ${ssl}/${dom}.key --cert ${ssl}/${dom}.crt`
-  }
-}
-
 const deployAll = async (argv: Arguments) => {
-  if (!ENV.isCI) {
-    await genDemoMtlsCertSecret()
-  }
   const output: ProcessOutputTrimmed = await hf(
     { fileOpts: 'helmfile.tpl/helmfile-init.yaml', args: 'template' },
     { streams: { stdout: debug.stream.debug } },
@@ -100,7 +74,6 @@ const deployAll = async (argv: Arguments) => {
 
 export const apply = async (argv: Arguments, options?: PrepareEnvironmentOptions): Promise<void> => {
   await setup(argv, options)
-  await decrypt(argv)
   if (argv._[0] === 'deploy' || (!argv.label && !argv.file)) {
     debug.verbose('Start deploy')
     await deployAll(argv)
