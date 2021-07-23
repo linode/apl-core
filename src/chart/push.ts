8b0476033db6c790d1b122d389055ebe6fafd172
Marc Went <marc.went+git@redkubes.com>
2021-07-23 14:22:26 +0200

    feat: 🎸 Implement push function

--- src/chart/push.ts
+++ src/chart/push.ts
@@ -1,14 +1,16 @@
 #!/usr/bin/env -S node --no-warnings --experimental-specifier-resolution=node --loader ts-node/esm
 import { existsSync } from 'fs'
+import { rm } from 'fs/promises'
+import fetch from 'node-fetch'
 import { fileURLToPath } from 'url'
 import yargs, { Argv } from 'yargs'
-import { $ } from 'zx'
+import { $, cd } from 'zx'
 import { encrypt } from '../cmd/encrypt'
 import { genSops } from '../cmd/gen-sops'
 import { OtomiDebugger, terminal } from '../common/debug'
-import { BasicArguments, ENV, loadYaml } from '../common/no-deps'
+import { BasicArguments, ENV, loadYaml, readdirRecurse } from '../common/no-deps'
 import { cleanupHandler } from '../common/setup'
-import { getFilename } from './common'
+import { delay, getFilename } from './common'
 
 const fileName = getFilename(import.meta.url)
 let debug: OtomiDebugger
@@ -50,9 +52,40 @@ export const push = async (argv: Arguments): Promise<void> => {
   if (giteaEnabled) {
     debug.log('Waiting for gitea to come up')
     const domain = `gitea.${clusterDomain}`
+    const subsequentExists = 3
+    let count = 0
+    // Need to wait for 3 subsequent exists, since DNS doesn't always propagate equally
+    do {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const res = await fetch(domain, { redirect: 'follow' })
+        if (res.ok) {
+          count += 1
+        } else {
+          count = 0
+        }
+      } catch (_) {
+        count = 0
+      }
+      // eslint-disable-next-line no-await-in-loop
+      await delay(250)
+    } while (count < subsequentExists)
   }
-
-  await $`echo 1`
+  const currDir = ENV.PWD
+  cd(ENV.DIR)
+  await $`git add -A`
+  await $`git commit --no-verify -m 'automated commit of otomi-values'`
+  const pushResult = await $`git push --set-upstream origin ${branch}`
+  cd(currDir)
+  if (pushResult.exitCode !== 0) {
+    debug.exit(1, pushResult.stderr)
+  } else {
+    debug.log('Done')
+  }
+  const decFiles = (await readdirRecurse(`${ENV.DIR}/env`)).filter(
+    (file) => file.endsWith('.yaml.dec') && file.includes('/secrets'),
+  )
+  await Promise.all(decFiles.map(async (file) => rm(file, { force: true })))
 }
 
 export const module = {
