d02dd0cc5e13cbf69ae5dfc383bcdf9c9a7cd253
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2021-08-02 11:51:20 +0200

    fix: add env for testing purpose

--- bin/ci-tests.sh
+++ bin/ci-tests.sh
@@ -7,6 +7,7 @@ set -e
 . bin/common.sh
 
 testEnv=$PWD/tests/fixtures
+source $testEnv/.env
 echo "Validating $testEnv values"
 
 ln -s $testEnv env
@@ -14,6 +15,10 @@ bats -T bin/tests
 opa test policies -v
 bin/validate-values.sh
 hf lint
+
+export AZURE_CLIENT_ID='AZURE_CLIENT_ID'
+export AZURE_CLIENT_SECRET='AZURE_CLIENT_SECRET'
+
 bin/validate-templates.sh
 # bin/check-policies.sh
 unlink env
