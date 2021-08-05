71b700aaecd78d92c334c23142d12ad0591ab41f
Mojtaba Imani <sm.imani@gmail.com>
2021-08-05 10:12:52 +0200

    fix: removing empty keys from value skeleton (#504)
    * fix: removing empty keys from value skeleton
    
    * fix: removing unused cluster env

--- chart/otomi/localtest.sh
+++ chart/otomi/localtest.sh
@@ -6,14 +6,14 @@ set -e
 function run_core() {
   image=$1
   shift
-  docker run --rm -it --env-file=../.env -e VERBOSE=1 -e IN_DOCKER=1 -e CI=1 -e OTOMI_VERSION=$OTOMI_VERSION -e OTOMI_VALUES_INPUT=/secret/values.yaml -w ${WORKDIR:-$PWD} -e ENV_DIR=/home/app/stack/env -v $ENV_OUT:/home/app/stack/env -v $PWD:$PWD -v $VALUES_DIR:/secret -v /tmp:/tmp $image "$@"
+  docker run --rm -it --env-file=$HOME/opt/.sops-secrets -e VERBOSE=1 -e IN_DOCKER=1 -e CI=1 -e OTOMI_VERSION=$OTOMI_VERSION -e OTOMI_VALUES_INPUT=/secret/values.yaml -w ${WORKDIR:-$PWD} -e ENV_DIR=/home/app/stack/env -v $ENV_OUT:/home/app/stack/env -v $PWD:$PWD -v $VALUES_DIR:/secret -v /tmp:/tmp $image "$@"
 }
 
 function run_task() {
   docker run --rm -it -e OTOMI_ENV_DIR=/env -e IN_DOCKER=1 -e CI=1 -e OTOMI_VALUES_INPUT=/secret/values.yaml -e OTOMI_SCHEMA_PATH=/env/values-schema.yaml -v $ENV_OUT:/env -v $VALUES_DIR:/secret -v /tmp:/tmp otomi/tasks:master "$@"
 }
 
-coreTag=chart-schema
+coreTag=master
 
 run_core otomi/core:$coreTag bash -c "$(cat chart/otomi/scripts/bootstrap-values.sh)"
 
