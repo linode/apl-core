f8d4467d7dbb32faec219595d36f601dae612676
Mojtaba <sm.imani@gmail.com>
2021-08-25 13:31:46 +0200

    feat: adds unit test for gen-secrets.ts

---
+++ test/src/cmd/lib/gen-secrets.test.ts
@@ -0,0 +1,21 @@
+import $RefParser from '@apidevtools/json-schema-ref-parser'
+import { strictEqual } from 'assert'
+import yaml from 'js-yaml'
+import { has, omit } from 'lodash-es'
+import { extractSecrets, generateSecrets } from '../../../../src/cmd/lib/gen-secrets'
+import { loadYaml } from '../../../../src/common/utils'
+
+describe('Automatic password generation tests', () => {
+  it(`Generated secrets object contains all expected generated secrets`, async () => {
+    const str = await generateSecrets()
+    const generatedSecrets = yaml.load(str)
+    strictEqual(has(generatedSecrets, 'charts.drone.adminToken'), true)
+  })
+  it('extractSecrets returns all secret pathes from schema', async () => {
+    const schema = loadYaml('values-schema.yaml')
+    const derefSchema = await $RefParser.dereference(schema)
+    const cleanSchema = omit(derefSchema, ['definitions', 'properties.teamConfig'])
+    const allSecrets = extractSecrets(cleanSchema).map((x) => x.Address)
+    strictEqual(allSecrets.includes('charts.drone.adminTokendd'), true)
+  })
+})
