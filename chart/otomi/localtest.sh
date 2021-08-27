bc0f704ca68c4c3b4c75bce4de0b05517eaf9a27
Maurice Faber <maurice.faber@redkubes.com>
2021-08-27 17:57:22 +0200

    fix: localtest deps

--- chart/otomi/localtest.sh
+++ chart/otomi/localtest.sh
@@ -1,7 +1,9 @@
 set -e
 
-export VALUES_INPUT=/tmp/otomi/secret/values.yaml
+export VALUES_INPUT=/tmp/otomi/values.yaml
 export CI=1
+export VERBOSITY=2
+# export GCLOUD_SERVICE_KEY=
 
 binzx/otomi bootstrap
-binzx/otomi apply
+# binzx/otomi apply
