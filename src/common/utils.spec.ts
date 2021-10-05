57358bf87b59921060cce12a32d11a93016cf280
Marc Went <marc.went+git@redkubes.com>
2021-10-05 15:15:25 +0200

    test: initial implementation of first unit test. TBC

--- src/common/utils.spec.ts
+++ src/common/utils.spec.ts
@@ -1,8 +1,27 @@
 import { strictEqual } from 'assert'
-import { has } from 'lodash-es'
-import { generateSecrets } from './utils.js'
+import { has } from 'lodash'
+import * as sinon from 'sinon'
+import * as utils from './utils'
+import { generateSecrets, GucciOptions } from './utils'
 
 describe('Automatic password generation tests', () => {
+  beforeEach(async () => {
+    sinon
+      .stub(utils, 'gucci')
+      .callsFake(
+        async (
+          tmpl: string | unknown,
+          args: { [key: string]: any },
+          opts?: GucciOptions,
+        ): Promise<string | Record<string, unknown>> => {
+          return {}
+        },
+      )
+  })
+  afterEach(() => {
+    sinon.reset()
+    // td.reset()
+  })
   it(`Generated secrets object contains all expected generated secrets`, async () => {
     const generatedSecrets = await generateSecrets({})
     strictEqual(has(generatedSecrets, 'charts.drone.adminToken'), true)
