4cdc32b7abb395ed3d350d6b42ad261859c43867
Maurice Faber <maurice.faber@redkubes.com>
2021-10-12 19:58:13 +0200

    fix: removed unneeded enforcement of .secrets file

--- src/common/crypt.ts
+++ src/common/crypt.ts
@@ -29,7 +29,7 @@ const preCrypt = (): void => {
   if (!env.CI) {
     const secretPath = `${env.ENV_DIR}/.secrets`
     if (!existsSync(secretPath)) {
-      throw new Error(`Expecting ${secretPath} to exist and hold credentials for SOPS!`)
+      debug.warn(`Expecting ${secretPath} to exist and hold credentials for SOPS. Not needed if already exists in env.`)
     }
   }
 }
