5a0b31cd085370df7c2abfde09889b31ef9ad3c3
Sebastiaan Verbeek <sebastiaan.verbeek@redkubes.com>
2021-11-10 10:42:28 +0100

    fix: make apply work

--- chart/otomi/quickstart/install.sh
+++ chart/otomi/quickstart/install.sh
@@ -5,18 +5,14 @@ set -e
 # minikube start --driver docker --network minikube --kubernetes-version=v1.19.0
 # docker network inspect minikube
 
-export VALUES_INPUT=./chart/otomi/quickstart/values.yaml
 export VERBOSITY=2
 export ENV_DIR=/tmp/otomi/values
-export OTOMI_TAG=master
+export OTOMI_TAG=quickstart-values
 
-rm -rf $ENV_DIR
-binzx/otomi bootstrap
+KUBECONFIG_MINIKUBE=~/.kube/minikube-flattened-config
+kubectl config view --flatten=true > $KUBECONFIG_MINIKUBE
+export KUBECONFIG=$KUBECONFIG_MINIKUBE
+kubectl config set-cluster minikube --server=https://minikube:8443 --insecure-skip-tls-verify
 
-# KUBECONFIG_MINIKUBE=~/.kube/minikube-flattened-config
-# kubectl config set-cluster minikube --server=https://localhost:52905
-# kubectl config view --flatten=true > $KUBECONFIG_MINIKUBE
-# export KUBECONFIG=$KUBECONFIG_MINIKUBE
-
-# export CUSTOM_NETWORK='--network minikube'
-# binzx/otomi apply
+export CUSTOM_NETWORK='--network minikube'
+binzx/otomi apply
