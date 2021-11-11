b908f88e94137ab54f7896e498431939c4aead52
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2021-11-11 18:29:06 +0100

    fix: wait until DNS recors propagate to the cluster DNS
    fix unassigned-issues#352

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -525,6 +525,8 @@ export const getOtomiLoadBalancerIP = async (): Promise<string> => {
 
   if (firstIngressData.ip) return firstIngressData.ip
   if (firstIngressData.hostname) {
+    // Wait until DNS records are propagated to the cluster DNS
+    await waitTillAvailable(firstIngressData.hostname)
     const resolveData = await resolveAny(firstIngressData.hostname)
     const resolveDataFiltered = resolveData.filter((val) => val.type === 'A' || val.type === 'AAAA') as (
       | AnyARecord
