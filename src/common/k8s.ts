288a254704238c6caa715ee95bb513b100568c95
Maurice Faber <maurice.faber@redkubes.com>
2021-11-22 14:33:09 +0100

    fix: pruning ghost secrets and added simple test

--- src/common/k8s.ts
+++ src/common/k8s.ts
@@ -2,10 +2,12 @@ import { CoreV1Api, KubeConfig } from '@kubernetes/client-node'
 
 let apiClient: CoreV1Api
 
-export function getApiClient(): CoreV1Api {
-  if (apiClient) return apiClient
-  const kc = new KubeConfig()
-  kc.loadFromDefault()
-  apiClient = kc.makeApiClient(CoreV1Api)
-  return apiClient
+export default {
+  getApiClient: (): CoreV1Api => {
+    if (apiClient) return apiClient
+    const kc = new KubeConfig()
+    kc.loadFromDefault()
+    apiClient = kc.makeApiClient(CoreV1Api)
+    return apiClient
+  },
 }
