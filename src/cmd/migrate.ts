956bbb534bda65f22b9d792009cd19d859777e76
svatwork <ajsverbeek@gmail.com>
2021-11-25 10:46:08 +0000

    feat: extend yargs builder

--- src/cmd/migrate.ts
+++ src/cmd/migrate.ts
@@ -15,9 +15,6 @@ import { writeValuesToFile } from '../common/values'
 interface Arguments extends BasicArguments {
   file?: string
   valuesFilePath?: string
-  applyDeletions?: boolean
-  applyLocations?: boolean
-  applyMutations?: boolean
   jsonPathExpr?: string
   postJsonPathExpr?: string
 }
@@ -25,7 +22,7 @@ interface Arguments extends BasicArguments {
 const cmdName = getFilename(__filename)
 const debug: OtomiDebugger = terminal(cmdName)
 
-export const deletionInPlace = async (): Promise<void> => {
+export const migrateDelete = async (): Promise<void> => {
   const argv: Arguments = getParsedArgs()
   if (argv.file && typeof argv.file === 'string') {
     const yaml = loadYaml(argv.file)
@@ -36,58 +33,74 @@ export const deletionInPlace = async (): Promise<void> => {
       if (unset(yaml, argv.jsonPathExpr)) {
         debug.info('New yaml:')
         debug.info(yaml)
+        await writeValuesToFile(argv.file, yaml as Record<string, any>) // I don't understand values.ts line 42
       }
     }
-    await writeValuesToFile(argv.file, yaml as Record<string, any>, true)
   }
 }
 
 // export const migrate = (): void => {}
 
 export const module = {
-  command: `${cmdName} [args..]`,
-  describe: 'migrate',
-  builder: (parser: Argv): Argv =>
-    parser.options({
-      file: {
-        alias: ['f'],
-        string: true,
-      },
-      'values-file-path': {
-        alias: ['S'],
-        string: true,
-      },
-      'apply-deletions': {
-        alias: ['d'],
-        boolean: true,
-        default: false,
-      },
-      'apply-locations': {
-        alias: ['l'],
-        boolean: true,
-        default: false,
-      },
-      'apply-mutations': {
-        alias: ['m'],
-        boolean: true,
-        default: false,
-      },
-      'json-path-expr': {
-        alias: ['expr'],
-        string: true,
-        conflicts: ['values-file-path'],
-      },
-      'post-json-path-expr': {
-        alias: ['post-expr'],
-        string: true,
-        conflicts: ['values-file-path', 'apply-deletions'],
-      },
-    }),
+  command: `${cmdName} [subcmd] [options]`,
+  description: `Migrates otomi-values according to Otomi values-schema evolution.\n This command is suitable for prototyping jsonpath-like queries.`,
+  builder: (parser): Argv => {
+    return parser
+      .command({
+        command: 'delete',
+        description: '',
+        builder: (): Argv => parser,
+        handler: async (argv) => {
+          setParsedArgs(argv)
+          await prepareEnvironment({ skipKubeContextCheck: true })
+          await migrateDelete()
+        },
+      })
+      .command({
+        command: 'move',
+        description: '',
+        builder() {
+          console.log('builder barr!')
+        },
+        handler: (a) => {
+          console.log('handler barr!')
+        },
+      })
+      .command({
+        command: 'mutate',
+        description: '',
+        builder() {
+          console.log('builder barr!')
+        },
+        handler: (a) => {
+          console.log('handler barr!')
+        },
+      })
+      .options({
+        file: {
+          alias: ['f'],
+          string: true,
+        },
+        'values-file-path': {
+          alias: ['V'],
+          string: true,
+        },
+        'json-path-expr': {
+          alias: ['expr'],
+          string: true,
+          conflicts: ['values-file-path'],
+        },
+        'post-json-path-expr': {
+          alias: ['post-expr'],
+          string: true,
+          conflicts: ['values-file-path', 'apply-deletions'],
+        },
+      })
+  },
 
   handler: async (argv: Arguments): Promise<void> => {
     setParsedArgs(argv)
 
     await prepareEnvironment({ skipKubeContextCheck: true })
-    if (argv.applyDeletions) deletionInPlace()
   },
 }
