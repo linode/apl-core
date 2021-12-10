99f9acc6b8cc3306e8bb4f575595603e9a7d80db
Maurice Faber <maurice.faber@redkubes.com>
2021-12-10 04:26:00 +0100

    fix: check-policies, violations, simplified otomi test

--- src/cmd/check-policies.ts
+++ src/cmd/check-policies.ts
@@ -1,4 +1,4 @@
-import { rmSync } from 'fs'
+import { existsSync, mkdirSync, readFileSync, rmSync, writeFileSync } from 'fs'
 import { Argv } from 'yargs'
 import { $, nothrow } from 'zx'
 import { cleanupHandler, prepareEnvironment } from '../common/cli'
@@ -21,12 +21,16 @@ const setup = (argv: BasicArguments): void => {
   cleanupHandler(() => cleanup(argv))
 }
 
-const checkPolicies = async (): Promise<void> => {
+export const checkPolicies = async (): Promise<void> => {
   const argv: BasicArguments = getParsedArgs()
   setup(argv)
   debug.info('Policy checking STARTED')
 
   const policiesFile = `${env().ENV_DIR}/env/policies.yaml`
+  const parametersFile = `${outDir}/parameters.yaml`
+  if (!existsSync(outDir)) mkdirSync(outDir)
+  // the policy parameters file's root prop is 'policies:', but conftest expects it to be served with 'parameters:'
+  writeFileSync(parametersFile, readFileSync(policiesFile, 'utf8').replace('policies:', 'parameters:'))
   const settingsFile = `${env().ENV_DIR}/env/settings.yaml`
   const settings = loadYaml(settingsFile)
   if (settings?.otomi?.addons?.conftest && !settings?.otomi?.addons?.conftest.enabled) {
@@ -43,7 +47,7 @@ const checkPolicies = async (): Promise<void> => {
   debug.info('Checking manifest against policies')
   const confTestOutput = (
     await nothrow(
-      $`conftest test ${extraArgs} --fail-on-warn --all-namespaces -d ${policiesFile} -p policies ${outDir}`,
+      $`conftest test ${extraArgs} --fail-on-warn --all-namespaces -d ${parametersFile} -p policies ${outDir}`,
     )
   ).stdout
   const cleanConftest: string = confTestOutput
