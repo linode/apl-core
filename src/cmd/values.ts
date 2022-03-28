09e7ea42a74384f07bf6b8c0ccdf272c9d98bda0
Maurice Faber <maurice.faber@redkubes.com>
2022-03-28 17:56:25 +0200

    fix: revert values flag

--- src/cmd/values.ts
+++ src/cmd/values.ts
@@ -16,7 +16,7 @@ const values = async (): Promise<void> => {
   const d = terminal(`cmd:${cmdName}:values`)
   d.info('Get values')
   const argv: Arguments = getParsedArgs()
-  const hfVal = await hfValues({ filesOnly: true })
+  const hfVal = await hfValues({ filesOnly: argv.filesOnly })
 
   d.info('Print values')
   console.log(dump(hfVal))
