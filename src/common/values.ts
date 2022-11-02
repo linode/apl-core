2cf9047318e148a077b39e822fe23fb15d4d0c41
Maurice Faber <morriz@idiotz.nl>
2022-11-02 16:10:41 +0100

    fix: admin config

--- src/common/values.ts
+++ src/common/values.ts
@@ -142,7 +142,7 @@ export const writeValues = async (inValues: Record<string, any>, overwrite = fal
   const d = terminal('common:values:writeValues')
   d.debug('Writing values: ', inValues)
   hasSops = await pathExists(`${env.ENV_DIR}/.sops.yaml`)
-  const teamConfig = (await loadYaml(`${env.ENV_DIR}/env/teams.yaml`)) as Record<string, Record<string, any>>
+  const { teamConfig } = (await loadYaml(`${env.ENV_DIR}/env/teams.yaml`)) as Record<string, Record<string, any>>
   const values = overwrite ? inValues : { ...teamConfig, ...inValues }
   const teams = Object.keys(values.teamConfig as Record<string, any>)
   // creating secret files
