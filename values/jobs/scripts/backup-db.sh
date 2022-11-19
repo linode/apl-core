c6c57045e73e9930797b841e6c9bf2946e7d4dbb
srodenhuis <sander.rodenhuis@redkubes.com>
2022-11-19 14:02:27 +0100

    fix: job env

--- values/jobs/scripts/backup-db.sh
+++ values/jobs/scripts/backup-db.sh
@@ -1,3 +1,3 @@
 set -e
-echo "MAINTENANCE TASK: creating backup of registry db and storing it in Minio harbor bucket"
-pg_dump -O -Fc --no-owner | aws --endpoint-url $ENDPOINT s3 cp - s3://harbor/registry/backup_$(date +%F).dump
+echo "MAINTENANCE TASK: creating backup of $PGDATABASE db and storing it in Minio $BUCKET bucket"
+pg_dump -O -Fc --no-owner | aws --endpoint-url $ENDPOINT s3 cp - s3://$BUCKET/$POSTFIX/backup_$(date +%F).dump
