942ab9073a5df8f25b7330dec4bfbbf2f51b59dd
srodenhuis <sander.rodenhuis@redkubes.com>
2022-12-01 11:47:31 +0100

    fix: script

--- values/jobs/scripts/backup-db.sh
+++ values/jobs/scripts/backup-db.sh
@@ -1,4 +1,5 @@
 set -e
 set -o pipefail
-echo "MAINTENANCE TASK: creating backup of $PGDATABASE db and storing it in Minio $BUCKET bucket"
-pg_dump -O -Fc --no-owner | aws --endpoint-url $ENDPOINT s3 cp - s3://$BUCKET/$POSTFIX/backup_$(date +%Y%m%d%H%M%S%N).dump
+FILENAME="backup_$(date +%Y%m%d%H%M%S%N).dump"
+echo "MAINTENANCE TASK: creating backup of $PGDATABASE db and storing it in Minio $BUCKET bucket as $FILENAME"
+pg_dump -O -Fc --no-owner | aws --endpoint-url $ENDPOINT s3 cp - s3://$BUCKET/$POSTFIX/$FILENAME
