115ad248a18878ed26c120859f9ecc1458b28c43
Maurice Faber <morriz@idiotz.nl>
2022-12-21 16:19:16 +0000

    fix: scripts cleaned

--- vendor/kes-to-eso/migrate-post.sh
+++ vendor/kes-to-eso/migrate-post.sh
@@ -3,11 +3,9 @@ KES_NAMESPACE="vault"
 
 mkdir -p eso_files
 
-#Step 2 Generate ESO files and apply them
+# Generate ESO files and apply them
 ./kes-to-eso generate -i kes_files -o eso_files -n $KES_NAMESPACE
-
 kubectl apply -f eso_files
 
-# Step 4 - Update Ownership references
-./kes-to-eso apply --all-secrets --all-namespaces #
-# kestoeso apply -n changeme-my-target-ns -s my-secret-1,my-secret-2 # Alternative for people that want to do a step-by-step migration
+# Update Ownership references
+./kes-to-eso apply --all-secrets --all-namespaces
--- vendor/kes-to-eso/migrate-pre.sh
+++ vendor/kes-to-eso/migrate-pre.sh
@@ -1,8 +1,5 @@
 #! /bin/bash -e
-ESO_NAMESPACE="vault"
-# Have KES and ESO both installed
-#Step 0  manual step to get .yaml files for KES External Secrets
-## can be done with:
 mkdir -p kes_files
-mkdir -p eso_files
+
+# Get the old secrets and store in folder
 bash -c "$(kubectl get externalsecrets.kubernetes-client.io -A -o=jsonpath='{range .items[*]}{"kubectl get externalsecrets.kubernetes-client.io -o yaml -n "}{.metadata.namespace}{" "}{.metadata.name}{" >> kes_files/"}{.metadata.namespace}{"-"}{.metadata.name}{".yaml; "}{end}')"
