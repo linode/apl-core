cba916919f2852d07375081ba2a4bd5449987715
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2022-12-22 17:08:41 +0100

    feat: migration

--- vendor/kes-to-eso/migrate-post.sh
+++ vendor/kes-to-eso/migrate-post.sh
@@ -5,10 +5,10 @@ ESO_NAMESPACE="external-secrets"
 echo "Upgrade from KES to ESO"
 [[ $(helm status -n external-secrets external-secrets) && ! $(helm status -n vault external-secrets) ]] && echo "Skipping"
 
-mkdir -p eso_files
+# mkdir -p eso_files
 
-echo "Scaling down ESO"
-kubectl scale deployment -n $ESO_NAMESPACE external-secrets --replicas=0
+# echo "Scaling down ESO"
+# kubectl scale deployment -n $ESO_NAMESPACE external-secrets --replicas=0
 # Generate ESO files and apply them
 # ./kestoeso generate -i kes_files -o eso_files -n $KES_NAMESPACE
 # kubectl apply -f eso_files
@@ -19,8 +19,8 @@ kubectl scale deployment -n $KES_NAMESPACE kubernetes-external-secrets --replica
 # Update Ownership references
 echo "Updating secrets ownership"
 ./kestoeso apply --all-secrets --all-namespaces
-echo "Scaling up ESO"
-kubectl scale deployment -n $ESO_NAMESPACE external-secrets --replicas=1
+# echo "Scaling up ESO"
+# kubectl scale deployment -n $ESO_NAMESPACE external-secrets --replicas=1
 echo "Uninstalling KES"
 helm uninstall -n vault external-secrets
 
