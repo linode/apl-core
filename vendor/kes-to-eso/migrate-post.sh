f2906e3583c0fd16e5750e6b428fc9470ae27223
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2023-01-02 14:40:55 +0100

    feat: migration

--- vendor/kes-to-eso/migrate-post.sh
+++ vendor/kes-to-eso/migrate-post.sh
@@ -22,9 +22,33 @@ echo "Upgrade from KES to ESO"
 echo "Scaling down KES"
 kubectl scale deployment -n $KES_NAMESPACE external-secrets --replicas=0
 
+# workaround for kestoeso
+
+cat >"$HOME"/.kube/config <<EOF
+apiVersion: v1
+clusters:
+- cluster:
+    server: "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT_HTTPS"
+    certificate-authority-data: "$(cat /var/run/secrets/kubernetes.io/serviceaccount/ca.crt | base64 -w0)"
+  name: default
+contexts:
+- context:
+    cluster: default
+    user: default
+  name: default
+current-context: default
+kind: Config
+users:
+- name: default
+  user:
+    token: $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+EOF
+
+chmod 644 "$HOME"/.kube/config
 # Update Ownership references
+
 echo "Patching secrets ownership KES to ESO"
-./"${scriptDir}"/kestoeso apply --all-secrets --all-namespaces
+./"${scriptDir}"/kestoeso apply --all-secrets --all-namespaces --kubeconfig
 # echo "Scaling up ESO"
 # kubectl scale deployment -n $ESO_NAMESPACE external-secrets --replicas=1gps
 echo "Removing KES CR"
