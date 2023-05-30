792a1df96f608551db8230784816037dd63e6b04
Cas Lubbers <cas.lubbers@redkubes.com>
2023-05-30 12:52:39 +0200

    feat(harbor): edit upgrade script so harbor database, secret and service persists

--- upgrades/pre/upgrade-0-22-0.sh
+++ upgrades/pre/upgrade-0-22-0.sh
@@ -5,3 +5,6 @@ set -eu
 [[ ! $(helm status -n harbor harbor) ]] && echo "Harbor doesn't exist. Skipping" && exit 0
 
 kubectl -n harbor scale deploy/harbor-core --replicas 0
+kubectl annotate -n harbor secret/harbor-database helm.sh/resource-policy='keep'
+kubectl annotate -n harbor sts/harbor-database helm.sh/resource-policy='keep'
+kubectl annotate -n harbor svc/harbor-database helm.sh/resource-policy='keep'
