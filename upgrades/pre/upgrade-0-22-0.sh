7824f7276351ccc41f4b60fff2ad5a7e71fc5667
Cas Lubbers <cas.lubbers@redkubes.com>
2023-06-22 11:32:33 +0200

    fix: update how harbor imports otomiDb

--- upgrades/pre/upgrade-0-22-0.sh
+++ upgrades/pre/upgrade-0-22-0.sh
@@ -4,7 +4,6 @@ set -eu
 
 [[ ! $(helm status -n harbor harbor) ]] && echo "Harbor doesn't exist. Skipping" && exit 0
 
-kubectl -n harbor scale deploy harbor-core harbor-portal --replicas 0
 kubectl annotate -n harbor secret/harbor-database helm.sh/resource-policy='keep' deprecated=true
 kubectl annotate -n harbor sts/harbor-database helm.sh/resource-policy='keep' deprecated=true
 kubectl annotate -n harbor svc/harbor-database helm.sh/resource-policy='keep' deprecated=true
