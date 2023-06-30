b9a76c53bf6b380426a1cd15814ae4b562ef2cf2
Ani Argjiri <ani.argjiri@redkubes.com>
2023-06-30 17:36:16 +0200

    fix: testing update script

--- upgrades/pre/upgrade-0-22-0.sh
+++ upgrades/pre/upgrade-0-22-0.sh
@@ -9,4 +9,4 @@ helm uninstall -n kube-system kured
 
 [[ ! $(helm status -n monitoring prometheus-operator) ]] && echo "The old prometheus-operator release does not exists. Skipping" && exit 0
 # It is safe to uninstall the release as it is stateless app.
-helm uninstall -n monitoring prometheus-operator
+helm uninstall -n monitoring prometheus-operator && sleep 10
