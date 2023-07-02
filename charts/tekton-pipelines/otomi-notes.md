9c47cbc71462dc0cd5854edddc010bb3becb4806
srodenhuis <sander.rodenhuis@redkubes.com>
2023-07-02 12:01:40 +0200

    fix: psp tekton

---
+++ charts/tekton-pipelines/otomi-notes.md
@@ -0,0 +1,2 @@
+Upstream chart changes:
+- Removed tekton-pipelines-podsecuritypolicy.yaml template
--- upgrades/pre/upgrade-0-22-0.sh
+++ upgrades/pre/upgrade-0-22-0.sh
@@ -10,3 +10,9 @@ helm uninstall -n kube-system kured
 [[ ! $(helm status -n monitoring prometheus-operator) ]] && echo "The old prometheus-operator release does not exists. Skipping" && exit 0
 # It is safe to uninstall the release as it is stateless app.
 helm uninstall -n monitoring prometheus-operator
+
+[[ ! $(helm status -n tekton-pipeline tekton) ]] && echo "The old tekton release does not exists. Skipping" && exit 0
+# It is safe to uninstall the release as it is stateless app.
+helm uninstall -n tekton-pipeline tekton
+kubectl delete ns tekton
+kubectl delete ns tekton-pipeline
