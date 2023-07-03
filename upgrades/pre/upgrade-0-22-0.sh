4af04356bb13a78bdeaca3bc5ace551cce9235a6
Ani Argjiri <ani.argjiri@redkubes.com>
2023-07-03 11:47:36 +0200

    feat: testing upgrade script

--- upgrades/pre/upgrade-0-22-0.sh
+++ upgrades/pre/upgrade-0-22-0.sh
@@ -8,3 +8,6 @@
 [[ $(helm status -n monitoring prometheus-operator 2>/dev/null) ]] && helm uninstall -n monitoring prometheus-operator || echo "The old prometheus-operator release does not exists. Skipping"
 # It is safe to uninstall the release as it is stateless app.
 
+[[ $(helm status -n monitoring prometheus-blackbox-exporter 2>/dev/null) ]] && helm uninstall -n monitoring prometheus-blackbox-exporter || echo "The old prometheus-blackbox-exporter release does not exists. Skipping"
+# It is safe to uninstall the release as it is stateless app.
+
