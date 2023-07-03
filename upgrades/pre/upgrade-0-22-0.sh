940cb9cc6551ec689ffbdb63ea9166689661168b
Ani Argjiri <ani.argjiri@redkubes.com>
2023-07-03 14:15:18 +0200

    fix: updated upgrade script

--- upgrades/pre/upgrade-0-22-0.sh
+++ upgrades/pre/upgrade-0-22-0.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-#set -eu
+set -eu
 
 [[ $(helm status -n kube-system kured 2>/dev/null) ]] && helm uninstall -n kube-system kured || echo "The old kured release does not exists. Skipping" 
 # It is safe to uninstall the release as it is stateless app.
 
 [[ $(helm status -n monitoring prometheus-operator 2>/dev/null) ]] && helm uninstall -n monitoring prometheus-operator || echo "The old prometheus-operator release does not exists. Skipping"
-# It is safe to uninstall the release as it is stateless app.
-
-[[ $(helm status -n monitoring prometheus-blackbox-exporter 2>/dev/null) ]] && helm uninstall -n monitoring prometheus-blackbox-exporter || echo "The old prometheus-blackbox-exporter release does not exists. Skipping"
 # It is safe to uninstall the release as it is stateless app.
 
