ca4f881b91dd89ad622a8e9a40206c79d3229886
Ani Argjiri <ani.argjiri@redkubes.com>
2023-07-11 11:55:04 +0200

    feat: updated upgrade script

--- upgrades/pre/upgrade-0-22-2.sh
+++ upgrades/pre/upgrade-0-22-2.sh
@@ -7,3 +7,9 @@ if [[ $(helm status -n istio-system oauth2-proxy 2>/dev/null) ]]; then
 else
   echo "The old oauth2-proxy release does not exists. Skipping"
 fi
+
+if [[ $(helm status -n istio-system oauth2-proxy-redis 2>/dev/null) ]]; then
+  helm uninstall -n istio-system oauth2-proxy-redis
+else
+  echo "The old oauth2-proxy-redis release does not exists. Skipping"
+fi
