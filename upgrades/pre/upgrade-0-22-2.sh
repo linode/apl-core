1964294267e3ab9ea31311b43986c9e9b890f5a7
Ani Argjiri <ani.argjiri@redkubes.com>
2023-07-13 10:14:20 +0200

    fix: testing upgrade script

--- upgrades/pre/upgrade-0-22-2.sh
+++ upgrades/pre/upgrade-0-22-2.sh
@@ -3,7 +3,7 @@
 set -eu
 
 if [[ $(helm status -n cert-manager cert-manager 2>/dev/null) ]]; then
-  kubectl delete -n cert-manager deployment cert-webhook cert-manager
+  kubectl delete -n cert-manager deployment cert-manager-cainjector
 else
   echo "The old cert-manager release does not exists. Skipping"
 fi
