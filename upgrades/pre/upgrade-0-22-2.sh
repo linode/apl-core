05bf10006c3db7747b37515655a807435ba255a4
Ani Argjiri <ani.argjiri@redkubes.com>
2023-07-14 11:27:59 +0200

    fix: updated upgrade script

--- upgrades/pre/upgrade-0-22-2.sh
+++ upgrades/pre/upgrade-0-22-2.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-if [[ $(helm status -n cert-manager cert-manager 2>/dev/null) ]]; then
+if [[ $(kubectl get deployment -n cert-manager cert-manager -o jsonpath='{.metadata.labels.app\.kubernetes\.io/version}') == "v1.10.0" ]]; then
   kubectl delete -n cert-manager deployment cert-manager-cainjector cert-manager-webhook cert-manager
 else
   echo "The old cert-manager release does not exists. Skipping"
