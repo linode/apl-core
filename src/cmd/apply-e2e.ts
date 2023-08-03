7fa9305070d9e03681b47f860cf94817f772a319
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-08-03 11:35:40 +0200

    test: apply e2e

--- src/cmd/apply-e2e.ts
+++ src/cmd/apply-e2e.ts
@@ -5,6 +5,7 @@ import { hf } from 'src/common/hf'
 import { getFilename, rootDir } from 'src/common/utils'
 import { HelmArguments, getParsedArgs, helmOptions, setParsedArgs } from 'src/common/yargs'
 import { Argv, CommandModule } from 'yargs'
+import { printWelcomeMessage } from './commit'
 
 const cmdName = getFilename(__filename)
 const dir = '/tmp/otomi/'
@@ -31,6 +32,7 @@ const applyE2e = async (): Promise<void> => {
     },
     { streams: { stdout: d.stream.log, stderr: d.stream.error } },
   )
+  await printWelcomeMessage()
 }
 
 export const module: CommandModule = {
