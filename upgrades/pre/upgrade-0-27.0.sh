7c364541a440e5244be9301d128ac55693b2e884
Ani Argjiri <ani.argjiri@redkubes.com>
2023-09-27 18:56:22 +0200

    feat: updated upgrade script

--- upgrades/pre/upgrade-0-27.0.sh
+++ upgrades/pre/upgrade-0-27.0.sh
@@ -2,4 +2,9 @@
 
 set -eu
 
-helm uninstall -n keycloak keycloak && sleep 30
+if [[ $(helm status -n keycloak keycloak 2>/dev/null) ]]; then
+    echo "Found old keycloak release. Will uninstall it..."
+    helm uninstall -n keycloak keycloak
+else
+    echo "Keycloak helm release not found"
+fi
