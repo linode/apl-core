d1ded820ce0851c5eb68774199499978318594fb
ferruh2021@gmail.com <ferruh.cihan@redkubes.com>
2023-10-30 13:08:43 +0100

    test: remove drone

--- src/cmd/gen-drone.ts
+++ src/cmd/gen-drone.ts
@@ -4,8 +4,7 @@ import { prepareEnvironment } from 'src/common/cli'
 import { terminal } from 'src/common/debug'
 import { hfValues } from 'src/common/hf'
 import { getFilename } from 'src/common/utils'
-import { getImageTag } from 'src/common/values'
-import { BasicArguments, getParsedArgs, setParsedArgs } from 'src/common/yargs'
+import { BasicArguments, setParsedArgs } from 'src/common/yargs'
 import { Argv } from 'yargs'
 
 export interface Arguments extends BasicArguments {
@@ -38,9 +37,8 @@ const getUrlKey = (provider) => {
 
 export const genDrone = async (envDir?: string): Promise<void> => {
   const d = terminal(`cmd:${cmdName}:genDrone`)
-  const argv: Arguments = getParsedArgs()
+  d.info('Drone Generating!')
   const allValues = (await hfValues(undefined, envDir)) as Record<string, any>
-  const branch = allValues.apps?.['otomi-api']?.git?.branch ?? 'main'
 
   const r = {
     alerts: {},
@@ -64,46 +62,7 @@ export const genDrone = async (envDir?: string): Promise<void> => {
       r[type][provider] = holder
     })
   })
-
-  const cluster = allValues.cluster?.name
-  const owner = allValues.cluster?.owner
-  const cloudProvider = allValues.cluster?.provider
-  const globalPullSecret = allValues.otomi?.globalPullSecret
-  const imageTag = await getImageTag(envDir)
-  const pullPolicy = imageTag.startsWith('v') ? 'if-not-exists' : 'always'
-  const requestsCpu = allValues.apps.drone.resources?.runner?.requests?.cpu
-  const requestsMem = allValues.apps.drone.resources?.runner?.requests?.memory
-
-  const obj = {
-    imageTag,
-    branch,
-    cluster,
-    cloudProvider,
-    globalPullSecret,
-    owner,
-    pullPolicy,
-    requestsCpu,
-    requestsMem,
-    r,
-  }
-
-  // const output = (await gucci(`${rootDir}/tpl/.drone.yml.gotmpl`, obj, true)) as string
-  // const output = 'drone output'
-
-  // // TODO: Remove when validate-values can validate subpaths
-  // if (!output) {
-  //   d.warn('Something went wrong trying to template using gucci')
-  //   return
-  // }
-
-  // if (argv.dryRun) {
-  //   d.log(output)
-  // } else {
-  //   const file = `${envDir ?? env.ENV_DIR}/.drone.yml`
-  //   await writeFile(file, output)
-  //   d.debug('.drone.yml: ', output)
-  //   d.log(`gen-drone is finished and the pipeline configuration is written to: ${file}`)
-  // }
+  d.info('Drone Generated!')
 }
 
 export const module = {
