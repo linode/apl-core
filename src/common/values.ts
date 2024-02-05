ab9ad34c40bf87bf6777ecfc00dc880574c069d0
ferruh2021@gmail.com <ferruh.cihan@redkubes.com>
2024-02-05 11:58:12 +0100

    test: bootstrap

--- src/common/values.ts
+++ src/common/values.ts
@@ -203,10 +203,10 @@ export const writeValues = async (inValues: Record<string, any>, overwrite = fal
     promises.push(writeValuesToFile(`${env.ENV_DIR}/env/secrets.settings.yaml`, secretSettings, overwrite))
   if (plainValues.cluster || overwrite)
     promises.push(writeValuesToFile(`${env.ENV_DIR}/env/cluster.yaml`, { cluster: plainValues.cluster }, overwrite))
-  if (plainValues.bootstrap || overwrite)
-    promises.push(
-      writeValuesToFile(`${env.ENV_DIR}/env/bootstrap.yaml`, { bootstrap: plainValues.bootstrap }, overwrite),
-    )
+  if (plainValues.bootstrap || overwrite) {
+    console.log('plainValues.bootstrap', plainValues.bootstrap)
+    promises.push(writeValuesToFile(`${env.ENV_DIR}/env/bootstrap.yaml`, { bootstrap: plainValues.bootstrap }, true))
+  }
   if (plainValues.policies || overwrite)
     promises.push(writeValuesToFile(`${env.ENV_DIR}/env/policies.yaml`, { policies: plainValues.policies }, overwrite))
   if (plainValues.teamConfig || overwrite) {
