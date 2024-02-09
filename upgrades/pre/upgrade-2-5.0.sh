027c8fbceceba9575ced8c67d706641449ad8b52
Ani Argjiri <ani.argjiri@redkubes.com>
2024-02-09 13:42:25 +0100

    feat: add annotation for volumesnapshotlocation in upgrade script

--- upgrades/pre/upgrade-2-5.0.sh
+++ upgrades/pre/upgrade-2-5.0.sh
@@ -5,6 +5,7 @@ set -eu
 if [[ $(helm status -n velero velero 2>/dev/null) ]]; then
     echo "Found old velero release. Will anotate otomi BackupStorageLocation CR."
     kubectl annotate -n velero backupstoragelocations.velero.io otomi meta.helm.sh/release-name=velero meta.helm.sh/release-namespace=velero
+    kubectl annotate -n velero volumesnapshotlocation.velerio.io otomi meta.helm.sh/release-name=velero meta.helm.sh/release-namespace=velero
 else
     echo "Velero helm release not found"
 fi
