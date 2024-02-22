30da1f9781661bf470b9e7edc4806d29d18aee05
Ani1357 <61050265+Ani1357@users.noreply.github.com>
2024-02-22 13:45:33 +0100

    fix: updated upgrade script (#1529)

--- upgrades/pre/upgrade-2-7-0.sh
+++ upgrades/pre/upgrade-2-7-0.sh
@@ -4,8 +4,8 @@ set -eu
 
 if [[ $(helm status -n velero velero 2>/dev/null) ]]; then
     echo "Found old velero release. Will anotate otomi BackupStorageLocation CR."
-    kubectl annotate -n velero backupstoragelocations.velero.io otomi meta.helm.sh/release-name=velero meta.helm.sh/release-namespace=velero
-    kubectl annotate -n velero volumesnapshotlocations.velero.io  otomi meta.helm.sh/release-name=velero meta.helm.sh/release-namespace=velero
+    kubectl annotate -n velero backupstoragelocations.velero.io meta.helm.sh/release-name=velero meta.helm.sh/release-namespace=velero
+    kubectl annotate -n velero volumesnapshotlocations.velero.io meta.helm.sh/release-name=velero meta.helm.sh/release-namespace=velero
 else
     echo "Velero helm release not found"
 fi
