ba7a16db2fbe0b1dff6873278b6fab7d7c49fe61
Ani Argjiri <ani.argjiri@redkubes.com>
2024-02-23 10:57:54 +0100

    fix: updated resource names in upgrade script

--- upgrades/pre/upgrade-2-7-0.sh
+++ upgrades/pre/upgrade-2-7-0.sh
@@ -5,7 +5,7 @@ set -eu
 if [[ $(helm status -n velero velero 2>/dev/null) ]]; then
   echo "Found old velero release. Upgrading Velero CRDs"
   kubectl annotate -n velero backupstoragelocations.velero.io otomi meta.helm.sh/release-name=velero meta.helm.sh/release-namespace=velero
-  kubectl annotate -n velero volumesnapshotlocations.velero.io default meta.helm.sh/release-name=velero meta.helm.sh/release-namespace=velero
+  kubectl annotate -n velero volumesnapshotlocations.velero.io otomi meta.helm.sh/release-name=velero meta.helm.sh/release-namespace=velero
   kubectl apply -f charts/velero/crds --server-side --force-conflicts
 else
   echo "Velero helm release not found. Skipping."
