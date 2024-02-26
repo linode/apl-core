06d13814cc1a8caf1d83d08a8a9a1f48358665aa
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2024-02-26 17:34:56 +0100

    feat: continue installation if the cluster validation cannnot be performed

--- src/cmd/validate-cluster.ts
+++ src/cmd/validate-cluster.ts
@@ -10,16 +10,21 @@ const cmdName = getFilename(__filename)
 export const validateCluster = async (): Promise<void> => {
   const d = terminal(`cmd:${cmdName}:validateCluster`)
   d.log('Cluster validation STARTED')
-  const result = await nothrow($`kubectl version -o json`)
-  const data = JSON.parse(result.stdout)
-  const k8sVersion: string = data.serverVersion.gitVersion.slice(1, 5)
-  if (!supportedK8sVersions.includes(k8sVersion)) {
-    const k8sVersions = supportedK8sVersions.join(', ')
-    d.error(
-      `The cluster with kubernetes version ${k8sVersion} is utilizing a less compatible version for otomi. For optimal performance and features, we recommend using one of our supported versions: [${k8sVersions}]. Make sure to align your cluster with these versions to take full advantage of our services.`,
-    )
-    process.exit(1)
-  } else d.log('Cluster validation SUCCESS')
+  try {
+    const result = await nothrow($`kubectl version -o json`)
+    const data = JSON.parse(result.stdout)
+    const k8sVersion: string = data.serverVersion.gitVersion.slice(1, 5)
+    if (!supportedK8sVersions.includes(k8sVersion)) {
+      const k8sVersions = supportedK8sVersions.join(', ')
+      d.error(
+        `The cluster with kubernetes version ${k8sVersion} is utilizing a less compatible version for otomi. For optimal performance and features, we recommend using one of our supported versions: [${k8sVersions}]. Make sure to align your cluster with these versions to take full advantage of our services.`,
+      )
+      process.exit(1)
+    } else d.log('Cluster validation SUCCESS')
+  } catch (e) {
+    d.warn(e)
+    d.warn('Could not validate cluster kubernetes version. Assuming that it is compatible. Proceeding.')
+  }
 }
 
 export const module = {
