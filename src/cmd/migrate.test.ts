69b7bbe3783cee3ded1390e0159d331bbe323611
Sander Rodenhuis <53382213+srodenhuis@users.noreply.github.com>
2024-03-14 17:27:13 +0100

    feat: make netpols independent of Services (#1503)
    Co-authored-by: ferruhcihan <ferruh.cihan@redkubes.com>
    Co-authored-by: Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>

--- src/cmd/migrate.test.ts
+++ src/cmd/migrate.test.ts
@@ -1,6 +1,10 @@
 import { applyChanges, Changes, filterChanges } from 'src/cmd/migrate'
 import stubs from 'src/test-stubs'
 
+jest.mock('uuid', () => ({
+  v4: jest.fn(() => 'my-fixed-uuid'),
+}))
+
 const { terminal } = stubs
 
 describe('Upgrading values', () => {
@@ -132,5 +136,282 @@ describe('Values migrations', () => {
       },
       true,
     )
+  })
+})
+
+const getMockValues = () => ({
+  teamConfig: {
+    teamA: {
+      selfService: { service: ['ingress', 'networkPolicy'] },
+      services: [
+        { id: 'my-fixed-uuid', name: 'svc1', networkPolicy: { ingressPrivate: { mode: 'AllowAll' } } },
+        {
+          id: 'my-fixed-uuid',
+          name: 'svc2',
+          networkPolicy: {
+            ingressPrivate: {
+              mode: 'AllowOnly',
+            },
+          },
+        },
+        {
+          id: 'my-fixed-uuid',
+          name: 'svc3',
+          networkPolicy: {
+            ingressPrivate: {
+              mode: 'DenyAll',
+            },
+          },
+        },
+        { id: 'my-fixed-uuid', name: 'svc4' },
+        { id: 'my-fixed-uuid', name: 'svc5', networkPolicy: { ingressPrivate: { mode: 'AllowAll' } } },
+        {
+          id: 'my-fixed-uuid',
+          name: 'svc6',
+          networkPolicy: {
+            ingressPrivate: {
+              mode: 'AllowOnly',
+              allow: [
+                {
+                  team: 'team1',
+                },
+                {
+                  team: 'team2',
+                  service: 'svc6',
+                },
+              ],
+            },
+          },
+        },
+      ],
+    },
+    teamB: {
+      services: [
+        {
+          id: 'my-fixed-uuid',
+          name: 'svc7',
+          networkPolicy: {
+            ingressPrivate: {
+              mode: 'AllowOnly',
+              allow: [
+                {
+                  team: 'team1',
+                },
+                {
+                  team: 'team2',
+                  service: 'svc7',
+                },
+              ],
+            },
+            egressPublic: [
+              {
+                domain: 'domain1.com',
+                ports: [
+                  {
+                    number: 8443,
+                    protocol: 'TCP',
+                  },
+                ],
+              },
+              {
+                domain: 'domain2.com',
+                ports: [
+                  {
+                    number: 443,
+                    protocol: 'HTTPS',
+                  },
+                ],
+              },
+              {
+                domain: '185.199.110.153',
+                ports: [
+                  {
+                    number: 443,
+                    protocol: 'TCP',
+                  },
+                ],
+              },
+              {
+                domain: 'ae::1',
+                ports: [
+                  {
+                    number: 443,
+                    protocol: 'TCP',
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+})
+
+const getExpectedValues = () => ({
+  teamConfig: {
+    teamA: {
+      selfService: { service: ['ingress'] },
+      services: [
+        { id: 'my-fixed-uuid', name: 'svc1' },
+        { id: 'my-fixed-uuid', name: 'svc2' },
+        { id: 'my-fixed-uuid', name: 'svc3' },
+        { id: 'my-fixed-uuid', name: 'svc4' },
+        { id: 'my-fixed-uuid', name: 'svc5' },
+        { id: 'my-fixed-uuid', name: 'svc6' },
+      ],
+      netpols: [
+        {
+          id: 'my-fixed-uuid',
+          name: 'svc1',
+          ruleType: {
+            type: 'ingress',
+            ingress: { mode: 'AllowAll', toLabelName: 'otomi.io/app', toLabelValue: 'svc1' },
+          },
+        },
+        {
+          id: 'my-fixed-uuid',
+          name: 'svc2',
+          ruleType: {
+            type: 'ingress',
+            ingress: { mode: 'AllowOnly', toLabelName: 'otomi.io/app', toLabelValue: 'svc2' },
+          },
+        },
+        {
+          id: 'my-fixed-uuid',
+          name: 'svc5',
+          ruleType: {
+            type: 'ingress',
+            ingress: { mode: 'AllowAll', toLabelName: 'otomi.io/app', toLabelValue: 'svc5' },
+          },
+        },
+        {
+          id: 'my-fixed-uuid',
+          name: 'svc6',
+          ruleType: {
+            type: 'ingress',
+            ingress: {
+              mode: 'AllowOnly',
+              allow: [
+                { fromNamespace: 'team-team1' },
+                { fromNamespace: 'team-team2', fromLabelName: 'otomi.io/app', fromLabelValue: 'svc6' },
+              ],
+              toLabelName: 'otomi.io/app',
+              toLabelValue: 'svc6',
+            },
+          },
+        },
+      ],
+    },
+    teamB: {
+      services: [{ id: 'my-fixed-uuid', name: 'svc7' }],
+      netpols: [
+        {
+          id: 'my-fixed-uuid',
+          name: 'svc7',
+          ruleType: {
+            type: 'ingress',
+            ingress: {
+              mode: 'AllowOnly',
+              allow: [
+                {
+                  fromNamespace: 'team-team1',
+                },
+                {
+                  fromNamespace: 'team-team2',
+                  fromLabelName: 'otomi.io/app',
+                  fromLabelValue: 'svc7',
+                },
+              ],
+              toLabelName: 'otomi.io/app',
+              toLabelValue: 'svc7',
+            },
+          },
+        },
+        {
+          id: 'my-fixed-uuid',
+          name: 'domain1-com',
+          ruleType: {
+            type: 'egress',
+            egress: {
+              domain: 'domain1.com',
+              ports: [
+                {
+                  number: 8443,
+                  protocol: 'TCP',
+                },
+              ],
+            },
+          },
+        },
+        {
+          id: 'my-fixed-uuid',
+          name: 'domain2-com',
+          ruleType: {
+            type: 'egress',
+            egress: {
+              domain: 'domain2.com',
+              ports: [
+                {
+                  number: 443,
+                  protocol: 'HTTPS',
+                },
+              ],
+            },
+          },
+        },
+        {
+          id: 'my-fixed-uuid',
+          name: '185-199-110-153',
+          ruleType: {
+            type: 'egress',
+            egress: {
+              domain: '185.199.110.153',
+              ports: [
+                {
+                  number: 443,
+                  protocol: 'TCP',
+                },
+              ],
+            },
+          },
+        },
+        {
+          id: 'my-fixed-uuid',
+          name: 'ae--1',
+          ruleType: {
+            type: 'egress',
+            egress: {
+              domain: 'ae::1',
+              ports: [
+                {
+                  number: 443,
+                  protocol: 'TCP',
+                },
+              ],
+            },
+          },
+        },
+      ],
+    },
+  },
+})
+
+describe('Network policies migrations', () => {
+  const values: any = getMockValues()
+  const valuesChanges: any = {
+    networkPoliciesMigration: true,
+  }
+  const deps: any = {
+    cd: jest.fn(),
+    rename: jest.fn(),
+    hfValues: jest.fn().mockReturnValue(values),
+    terminal,
+    writeValues: jest.fn(),
+  }
+  it('should apply changes to services and create netpols ', async () => {
+    await applyChanges([valuesChanges], false, deps)
+    const expectedValues = getExpectedValues()
+    expect(deps.writeValues).toBeCalledWith(expectedValues, true)
   })
 })
