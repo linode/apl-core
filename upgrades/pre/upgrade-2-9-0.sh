3d21bb683f28a0158acc22fc1c1fc1777dcf4317
Matthias Erll <matthias.erll@redkubes.com>
2024-04-03 15:31:44 +0200

    feat: include olm resource cleanup in upgrade script

--- upgrades/pre/upgrade-2-9-0.sh
+++ upgrades/pre/upgrade-2-9-0.sh
@@ -2,10 +2,16 @@
 
 set -eu
 
+if kubectl get crd | grep -q operators.coreos.com; then
+  kubectl delete apiservices.apiregistration.k8s.io v1.packages.operators.coreos.com
+  kubectl delete -f charts/operator-lifecycle-manager/crds
+else
+  echo "No OLM operators found. Skipping."
+fi
+
 if [[ $(helm status -n argocd argocd-operator-cr 2>/dev/null) ]]; then
   helm uninstall argocd-operator-cr -n argocd
   helm uninstall argocd-operator-artifacts -n argocd
-  helm uninstall argocd-operator -n argocd
   kubectl label --overwrite customresourcedefinitions.apiextensions.k8s.io "applications.argoproj.io" app.kubernetes.io/managed-by=Helm
   kubectl annotate customresourcedefinitions.apiextensions.k8s.io "applications.argoproj.io" meta.helm.sh/release-name=argocd meta.helm.sh/release-namespace=argocd
   kubectl label --overwrite customresourcedefinitions.apiextensions.k8s.io "applicationsets.argoproj.io" app.kubernetes.io/managed-by=Helm
