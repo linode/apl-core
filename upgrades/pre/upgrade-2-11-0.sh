daf9c23fa9e363b45d624eecc4fb23e1748478a8
Cas Lubbers <clubbers@korfinancial.com>
2024-04-22 10:15:40 +0200

    feat: add upgrade yaml to annotate gitea databases

--- upgrades/pre/upgrade-2-11-0.sh
+++ upgrades/pre/upgrade-2-11-0.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-kubectl annotate -n harbor secret/harbor-database helm.sh/resource-policy='keep' deprecated=true
-kubectl annotate -n harbor sts/harbor-database helm.sh/resource-policy='keep' deprecated=true
-kubectl annotate -n harbor svc/harbor-database helm.sh/resource-policy='keep' deprecated=true
+kubectl annotate -n harbor secret/gitea-database helm.sh/resource-policy='keep' deprecated=true
+kubectl annotate -n harbor sts/gitea-database helm.sh/resource-policy='keep' deprecated=true
+kubectl annotate -n harbor svc/gitea-database helm.sh/resource-policy='keep' deprecated=true
