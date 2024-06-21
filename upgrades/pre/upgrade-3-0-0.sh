07636a5c460f74f00661a81a1bed506f7d7b7aa0
Sander Rodenhuis <53382213+srodenhuis@users.noreply.github.com>
2024-06-21 11:24:31 +0200

    feat: kyverno policies (#1462)
    Co-authored-by: Sander Rodenhuis <sander.rodenhuis@redkubes.com>
    Co-authored-by: ferruhcihan <ferruh.cihan@redkubes.com>
    Co-authored-by: Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
    Co-authored-by: Matthias Erll <matthias.erll@redkubes.com>
    Co-authored-by: Matthias Erll <merll@akamai.com>
    Co-authored-by: Ani Argjiri <aargjiri@akamai.com>
    Co-authored-by: jeho <17126497+j-zimnowoda@users.noreply.github.com>
    Co-authored-by: Jehoszafat Zimnowoda <jzimnowo@akamai.com>

--- upgrades/pre/upgrade-3-0-0.sh
+++ upgrades/pre/upgrade-3-0-0.sh
@@ -5,3 +5,12 @@ set -eu
 kubectl annotate -n gitea secret/gitea-postgresql helm.sh/resource-policy='keep' deprecated=true
 kubectl annotate -n gitea sts/gitea-postgresql helm.sh/resource-policy='keep' deprecated=true
 kubectl annotate -n gitea svc/gitea-postgresql helm.sh/resource-policy='keep' deprecated=true
+
+
+if [[ $(helm status -n gatekeeper-system gatekeeper 2>/dev/null) ]]; then
+  helm uninstall -n gatekeeper-system gatekeeper
+  helm uninstall -n gatekeeper-system gatekeeper-artifacts
+  helm uninstall -n gatekeeper-system gatekeeper-constraints
+  helm uninstall -n gatekeeper-system opa-exporter-artifacts
+  helm uninstall -n opa-exporter opa-exporter
+fi
