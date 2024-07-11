33698f3bd67b2069fd70c24b04b664d858d13654
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-07-11 15:18:27 +0200

    test: upgrade files

--- upgrades/pre/upgrade-2-13-0.sh
+++ upgrades/pre/upgrade-2-13-0.sh
@@ -5,3 +5,8 @@ set -eu
 kubectl delete job gitea-prepare -n maintenance
 kubectl delete job harbor -n maintenance
 kubectl delete job keycloak -n maintenance
+
+if [[ $(helm status -n gitea-operator gitea-operator 2>/dev/null) ]]; then
+  helm uninstall -n gitea-operator gitea-operator
+  kubectl delete ns gitea-operator
+fi
