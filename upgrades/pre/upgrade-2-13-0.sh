9d8e80d3f764edae648306bb93c7458fd5b2f4a9
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-07-11 15:33:02 +0200

    test: operators cleanup

--- upgrades/pre/upgrade-2-13-0.sh
+++ upgrades/pre/upgrade-2-13-0.sh
@@ -2,9 +2,17 @@
 
 set -eu
 
-kubectl delete job gitea-prepare -n maintenance
-kubectl delete job harbor -n maintenance
-kubectl delete job keycloak -n maintenance
+if [[ $(kubectl get job gitea-prepare -n maintenance 2>/dev/null) ]]; then
+  kubectl delete job gitea-prepare -n maintenance
+fi
+
+if [[ $(kubectl get job harbor -n maintenance 2>/dev/null) ]]; then
+  kubectl delete job harbor -n maintenance
+fi
+
+if [[ $(kubectl get job keycloak -n maintenance 2>/dev/null) ]]; then
+  kubectl delete job keycloak -n maintenance
+fi
 
 if [[ $(helm status -n gitea-operator gitea-operator 2>/dev/null) ]]; then
   helm uninstall -n gitea-operator gitea-operator
