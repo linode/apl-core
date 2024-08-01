b0a00c7557330a9a8cc78c7b731beb2acc1c991c
Jehoszafat Zimnowoda <17126497+j-zimnowoda@users.noreply.github.com>
2024-08-01 16:50:25 +0200

    feat: add comape.sh script

--- bin/compare.sh
+++ bin/compare.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -ue
 
 # Usage: bin/compare.sh -l name=loki
@@ -18,8 +19,26 @@ git checkout $compareBranch
 helmfile template $templateArgs --output-dir-template="../$targetDirB/{{.Release.Namespace}}-{{.Release.Name}}"
 git checkout $currentBranch
 
-echo "###########################################################################################################################################"
+set +e
+diff_output=$(diff -q -r $targetDirA $targetDirB)
+set -e
+# Process each line of diff output
+
+echo "$diff_output" | while read -r line; do
+  # Check if the line indicates a difference
+  if [[ $line == *" and "* ]]; then
+    # Extract the paths using cut
+    first_path=$(echo $line | cut -d' ' -f2)
+    second_path=$(echo $line | cut -d' ' -f4)
+
+    # Use dyff to compare the files
+    dyff between "$first_path" "$second_path"
+  fi
+done
+
+echo "#########################################################"
 echo "#"
-echo "#  In VSCode select two directories $targetDirA and $targetDirB and right click and select the 'Compare selected folders' option"
+echo "#  Above YAML documents comparison produced by dyff tool."
+echo "# You can also select two directories in VSCode $targetDirA and $targetDirB and right click and select the 'Compare selected folders' option"
 echo "#"
-echo "###########################################################################################################################################"
+echo "#########################################################"
