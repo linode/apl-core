32337bf66aed9ec1463d5019dd6a91d18c8ca13c
Jehoszafat Zimnowoda <17126497+j-zimnowoda@users.noreply.github.com>
2024-08-30 10:25:41 +0200

    fix: rename variables

--- bin/compare.sh
+++ bin/compare.sh
@@ -6,27 +6,28 @@ set -ue
 export ENV_DIR=$PWD/tests/fixtures
 
 readonly templateArgs="$@"
-readonly currentBranch=$(git rev-parse --abbrev-ref HEAD)
-readonly compareBranch='main'
+readonly branchA='main'
+# branchB current branch
+readonly branchB=$(git rev-parse --abbrev-ref HEAD)
 
-targetDirA="tmp/${currentBranch}"
-targetDirB="tmp/${compareBranch}"
+targetDirA="tmp/${branchA}"
+targetDirB="tmp/${branchB}"
 
 export NODE_ENV=test
-helmfile template $templateArgs --output-dir-template="../$targetDirA/{{.Release.Namespace}}-{{.Release.Name }}"
+helmfile template $templateArgs --output-dir-template="../$targetDirB/{{.Release.Namespace}}-{{.Release.Name }}"
 
-git checkout $compareBranch
+git checkout $branchA
 # we remove previously rendered manifests so they are not mixed up with newly rendered
-rm -rf $targetDirB
-helmfile template $templateArgs --output-dir-template="../$targetDirB/{{.Release.Namespace}}-{{.Release.Name}}"
-git checkout $currentBranch
+rm -rf $targetDirA
+helmfile template $templateArgs --output-dir-template="../$targetDirA/{{.Release.Namespace}}-{{.Release.Name}}"
+git checkout $branchB
 
 # order of arguments matters so new chanages are green color
-bin/dyff.sh $targetDirB $targetDirA
+bin/dyff.sh $targetDirA $targetDirB
 
 echo "#########################################################"
 echo "#"
 echo "# Above YAML documents diff produced by dyff tool."
-echo "# You can also select two directories in VSCode $targetDirA and $targetDirB and right click and select the 'Compare selected folders' option"
+echo "# You can also select two directories in VSCode $targetDirB and $targetDirA and right click and select the 'Compare selected folders' option"
 echo "#"
 echo "#########################################################"
