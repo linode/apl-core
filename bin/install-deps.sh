565d851830a9c6af2c38d23694652ca3b580c9fe
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-09-12 09:11:55 +0200

    fix: helm secrets plugin version

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -5,7 +5,7 @@ go install github.com/plexsystems/konstraint@latest
 npm install -g json-dereference-cli
 
 helm plugin install https://github.com/databus23/helm-diff.git
-helm plugin install https://github.com/jkroepke/helm-secrets.git --version v3.15.
+helm plugin install https://github.com/jkroepke/helm-secrets.git --version v3.15.0
 
 echo "Set shell rc file:"
 echo 'echo  export PATH="$HOME/go/bin:$PATH" >> $HOME/.zshrc'
