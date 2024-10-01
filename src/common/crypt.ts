3ebe262e051a1a78da916286eac5583a2c6b9b54
Ani Argjiri <aargjiri@akamai.com>
2024-10-01 13:11:57 +0200

    feat: generate new core image

--- src/common/crypt.ts
+++ src/common/crypt.ts
@@ -15,8 +15,8 @@ export interface Arguments extends BasicArguments {
 EventEmitter.defaultMaxListeners = 20
 
 enum CryptType {
-  ENCRYPT = 'helm secrets encrypt',
-  DECRYPT = 'helm secrets decrypt',
+  ENCRYPT = 'helm secrets encrypt -i',
+  DECRYPT = 'helm secrets decrypt -i',
   ROTATE = 'sops --input-type=yaml --output-type=yaml -i -r',
 }
 
