c7aec39bfddf8a0f352dba4fdb76de411d842603
Jehoszafat Zimnowoda <17126497+j-zimnowoda@users.noreply.github.com>
2024-12-06 17:35:04 +0100

    ci: try catch

--- ci/src/update-helm-chart-deps.mjs
+++ ci/src/update-helm-chart-deps.mjs
@@ -52,101 +52,104 @@ async function main() {
       }
 
       console.log(`Checking updates for dependency: ${dependency.name}`)
+      try {
+        // Add the Helm repository (idempotent)
+        await $`helm repo add ${dependency.name} ${dependency.repository}`
+        await $`helm repo update`
 
-      // Add the Helm repository (idempotent)
-      await $`helm repo add ${dependency.name} ${dependency.repository}`
-      await $`helm repo update`
+        // Get all available versions for the dependency
+        const allVersions = await $`helm search repo ${dependency.name}/${dependency.name} -o json`
+          .then((output) => JSON.parse(output.stdout))
+          .then((results) => results.map((entry) => entry.version).filter((version) => semver.valid(version)))
 
-      // Get all available versions for the dependency
-      const allVersions = await $`helm search repo ${dependency.name}/${dependency.name} -o json`
-        .then((output) => JSON.parse(output.stdout))
-        .then((results) => results.map((entry) => entry.version).filter((version) => semver.valid(version)))
-
-      if (!allVersions.length) {
-        console.error(`No valid versions found for dependency ${dependency.name}`)
-        continue
-      }
-
-      // Filter versions for allowed upgrades (minor/patch)
-      const currentVersion = dependency.version
-      const filteredVersions = allVersions.filter((version) => {
-        if (semver.lte(version, currentVersion)) {
-          return false // Ignore versions that are <= current version
-        }
-        if (allowedUpgradeType === 'patch') {
-          return semver.diff(currentVersion, version) === 'patch'
-        }
-        if (allowedUpgradeType === 'minor') {
-          const isMinorOrPatch =
-            semver.diff(currentVersion, version) === 'patch' || semver.diff(currentVersion, version) === 'minor'
-          return isMinorOrPatch
-        }
-        return true // Default: Allow all upgrades
-      })
-
-      if (!filteredVersions.length) {
-        console.log(`No matching ${allowedUpgradeType} updates for dependency ${dependency.name}`)
-        continue
-      }
-
-      // Determine the latest matching version
-      const latestVersion = filteredVersions.sort(semver.rcompare)[0]
-
-      if (latestVersion === currentVersion) {
-        console.log(`${dependency.name} is already up to date.`)
-        continue
-      }
-      const branchName = `ci-update-${dependency.name}-to-${latestVersion}`
-      if (ciPushtoBranch) {
-        const remoteBranch = await $`git ls-remote --heads origin ${branchName}`
-        if (remoteBranch.message === '') {
-          console.log(
-            `Skipping  updates for dependency: ${dependency.name}: the remote branch ${branchName} already exists`,
-          )
+        if (!allVersions.length) {
+          console.error(`No valid versions found for dependency ${dependency.name}`)
           continue
         }
-      }
 
-      console.log(`Updating ${dependency.name} from version ${currentVersion} to ${latestVersion}`)
+        // Filter versions for allowed upgrades (minor/patch)
+        const currentVersion = dependency.version
+        const filteredVersions = allVersions.filter((version) => {
+          if (semver.lte(version, currentVersion)) {
+            return false // Ignore versions that are <= current version
+          }
+          if (allowedUpgradeType === 'patch') {
+            return semver.diff(currentVersion, version) === 'patch'
+          }
+          if (allowedUpgradeType === 'minor') {
+            const isMinorOrPatch =
+              semver.diff(currentVersion, version) === 'patch' || semver.diff(currentVersion, version) === 'minor'
+            return isMinorOrPatch
+          }
+          return true // Default: Allow all upgrades
+        })
 
-      // Update the version in Chart.yaml
-      dependency.version = latestVersion
+        if (!filteredVersions.length) {
+          console.log(`No matching ${allowedUpgradeType} updates for dependency ${dependency.name}`)
+          continue
+        }
 
-      const commitMessage = `chore(chart-deps): update ${dependency.name} to version ${latestVersion}`
-      if (ciCreateFeatureBranch) {
-        // Create a new branch for the update
-        await $`git checkout -b ${branchName}`
-      }
+        // Determine the latest matching version
+        const latestVersion = filteredVersions.sort(semver.rcompare)[0]
 
-      // Write the updated Chart.yaml file
-      const updatedChart = yaml.dump(chart)
-      await fs.writeFile(chartFile, updatedChart, 'utf8')
-      // Fetch and unpack the new chart version
-      const tempDir = `./tmp/charts/${dependency.name}`
-      await $`mkdir -p ${tempDir}`
-      await $`helm pull ${dependency.name}/${dependency.name} --version ${latestVersion} --destination ${tempDir}`
-      await $`rm -R ${chartsDir}/${dependency.name}`
-      await $`tar -xzvf ${tempDir}/${dependency.name}-${latestVersion}.tgz -C ${chartsDir}`
+        if (latestVersion === currentVersion) {
+          console.log(`${dependency.name} is already up to date.`)
+          continue
+        }
+        const branchName = `ci-update-${dependency.name}-to-${latestVersion}`
+        if (ciPushtoBranch) {
+          const remoteBranch = await $`git ls-remote --heads origin ${branchName}`
+          if (remoteBranch.message === '') {
+            console.log(
+              `Skipping  updates for dependency: ${dependency.name}: the remote branch ${branchName} already exists`,
+            )
+            continue
+          }
+        }
 
-      if (ciCreateFeatureBranch) {
-        await $`git add ${chartFile}`
-        await $`git add ${chartsDir}`
-        await $`git commit -m ${commitMessage}`
-      }
-      if (ciPushtoBranch) {
-        // Push the branch
-        await $`git push --no-verify origin ${branchName}`
-      }
-      if (ciCreateGithubPr) {
-        // Create a pull request
-        const prBody = `This PR updates the dependency **${dependency.name}** to version **${latestVersion}**.`
-        await $`gh pr create --title ${commitMessage} --body "${prBody}" --base ${baseBranch} --head ${branchName}`
-      }
+        console.log(`Updating ${dependency.name} from version ${currentVersion} to ${latestVersion}`)
 
-      if (ciCreateFeatureBranch) {
-        // Reset to the main branch for the next dependency
-        await $`git checkout ${baseBranch}`
-        await $`git reset --hard origin/${baseBranch}`
+        // Update the version in Chart.yaml
+        dependency.version = latestVersion
+
+        const commitMessage = `chore(chart-deps): update ${dependency.name} to version ${latestVersion}`
+        if (ciCreateFeatureBranch) {
+          await $`git checkout -b ${branchName}`
+        }
+
+        // Write the updated Chart.yaml file
+        const updatedChart = yaml.dump(chart)
+        await fs.writeFile(chartFile, updatedChart, 'utf8')
+        // Fetch and unpack the new chart version
+        const tempDir = `./tmp/charts/${dependency.name}`
+        await $`mkdir -p ${tempDir}`
+        await $`helm pull ${dependency.name}/${dependency.name} --version ${latestVersion} --destination ${tempDir}`
+        await $`rm -R ${chartsDir}/${dependency.name}`
+        await $`tar -xzvf ${tempDir}/${dependency.name}-${latestVersion}.tgz -C ${chartsDir}`
+
+        if (ciCreateFeatureBranch) {
+          await $`git add ${chartFile}`
+          await $`git add ${chartsDir}`
+          await $`git commit -m ${commitMessage}`
+        }
+        if (ciPushtoBranch) {
+          // Push the branch
+          await $`git push --no-verify origin ${branchName}`
+        }
+        if (ciCreateGithubPr) {
+          // Create a pull request
+          const prBody = `This PR updates the dependency **${dependency.name}** to version **${latestVersion}**.`
+          await $`gh pr create --title ${commitMessage} --body "${prBody}" --base ${baseBranch} --head ${branchName}`
+        }
+
+        if (ciCreateFeatureBranch) {
+          // Reset to the main branch for the next dependency
+          await $`git checkout ${baseBranch}`
+          await $`git reset --hard origin/${baseBranch}`
+        }
+      } catch (error) {
+        console.error('Error updating dependencies:', error)
+        continue
       }
     }
 
@@ -156,5 +159,4 @@ async function main() {
     process.exit(1)
   }
 }
-
 await main()
