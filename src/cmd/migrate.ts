7bee88be4a9b3af4104fde081be5edcbaa3c64f7
Ferruh <63190600+ferruhcihan@users.noreply.github.com>
2025-04-11 18:05:46 +0200

    feat: new build page (#2026)
    Co-authored-by: Dennis van Kekem <38350840+dennisvankekem@users.noreply.github.com>

--- src/cmd/migrate.ts
+++ src/cmd/migrate.ts
@@ -5,7 +5,7 @@ import { randomUUID } from 'crypto'
 import { diff } from 'deep-diff'
 import { copy, createFileSync, move, pathExists, renameSync, rm } from 'fs-extra'
 import { mkdir, readFile, writeFile } from 'fs/promises'
-import { cloneDeep, each, get, isUndefined, isObject, mapKeys, mapValues, omit, pick, pull, set, unset } from 'lodash'
+import { cloneDeep, each, get, isObject, isUndefined, mapKeys, mapValues, omit, pick, pull, set, unset } from 'lodash'
 import { basename, dirname, join } from 'path'
 import { prepareEnvironment } from 'src/common/cli'
 import { decrypt, encrypt } from 'src/common/crypt'
@@ -19,8 +19,7 @@ import { BasicArguments, getParsedArgs, setParsedArgs } from 'src/common/yargs'
 import { v4 as uuidv4 } from 'uuid'
 import { parse } from 'yaml'
 import { Argv } from 'yargs'
-import { $, cd } from 'zx'
-import { glob } from 'glob'
+import { $, cd, glob } from 'zx'
 const cmdName = getFilename(__filename)
 
 interface Arguments extends BasicArguments {
@@ -52,6 +51,7 @@ interface Change {
   }>
   networkPoliciesMigration?: boolean
   teamResourceQuotaMigration?: boolean
+  buildImageNameMigration?: boolean
 }
 
 export type Changes = Array<Change>
@@ -301,6 +301,35 @@ const networkPoliciesMigration = async (values: Record<string, any>): Promise<vo
   )
 }
 
+export const getBuildName = (name: string, tag: string): string => {
+  return `${name}-${tag}`
+    .toLowerCase()
+    .replace(/[^a-z0-9-]/gi, '-') // Replace invalid characters with hyphens
+    .replace(/-+/g, '-') // Replace multiple consecutive hyphens with a single hyphen
+    .replace(/^-|-$/g, '') // Remove leading or trailing hyphens
+}
+
+const buildImageNameMigration = async (values: Record<string, any>): Promise<void> => {
+  const teams: Array<string> = Object.keys(values?.teamConfig as Record<string, any>)
+  type Build = {
+    name: string
+    tag: string
+    imageName?: string
+  }
+  await Promise.all(
+    teams.map(async (teamName) => {
+      const builds: Build[] = get(values, `teamConfig.${teamName}.builds`, [])
+      if (!builds || builds.length === 0) return
+      for (const build of builds) {
+        set(build, 'imageName', build.name)
+        const buildName = getBuildName(build.name, build.tag)
+        set(build, 'name', buildName)
+        await deleteFile(`env/teams/${teamName}/builds/${build.imageName}.yaml`)
+      }
+    }),
+  )
+}
+
 const teamResourceQuotaMigration = (values: Record<string, any>) => {
   Object.entries(values?.teamConfig as Record<string, any>).forEach(([teamName, teamValues]) => {
     const resourceQuota = teamValues?.settings?.resourceQuota
@@ -381,8 +410,9 @@ export const applyChanges = async (
 
     if (c.networkPoliciesMigration) await networkPoliciesMigration(values)
     if (c.teamResourceQuotaMigration) teamResourceQuotaMigration(values)
+    if (c.buildImageNameMigration) await buildImageNameMigration(values)
 
-    Object.assign(values, { version: c.version })
+    Object.assign(values.versions, { specVersion: c.version })
   }
   if (!dryRun) await deps.writeValues(values, true)
   // @ts-ignore
