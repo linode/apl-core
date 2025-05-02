76773b00c211a3290bbc05f234f421527df99894
Cas Lubbers <clubbers@akamai.com>
2025-05-02 16:25:04 +0200

    chore(deps): update husky hooks and add babel and eslint configurations

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,17 @@
 import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
   roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/src/test-init.ts'],
   moduleDirectories: ['node_modules', __dirname],
   modulePathIgnorePatterns: ['src/cmd/test.ts'],
   testEnvironment: 'node',
   transform: {
-    '^.+.tsx?$': ['ts-jest', { compiler: 'ttypescript', tsconfig: 'tsconfig.jest.json' }],
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
   silent: false,
   testMatch: ['**/*.test.ts'],
   verbose: true,
 }
+
 export default config
