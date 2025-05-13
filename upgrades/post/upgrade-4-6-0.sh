d9b6e9241d8ae1f341eff3b86a67771ac97f3755
Matthias Erll <merll@akamai.com>
2025-05-13 16:40:36 +0200

    fix: continue if resource not found, run post upgrade

--- upgrades/post/upgrade-4-6-0.sh
+++ upgrades/post/upgrade-4-6-0.sh
@@ -2,14 +2,16 @@
 
 set -eu
 
-if [[ $(helm status -n istio-operator istio-operator-artifacts 2>/dev/null) ]]; then
-  helm uninstall -n istio-operator istio-operator-artifacts
+if [[ $(kubectl get applications.argoproj.io -n argocd istio-operator-istio-operator-artifacts 2>/dev/null) ]]; then
+  kubectl delete applications.argoproj.io -n argocd istio-operator-istio-operator-artifacts
+  helm uninstall -n istio-operator istio-operator-artifacts || true
 else
   echo "Istio Operator resource not deployed. Skipping"
 fi
 
-if [[ $(helm status -n istio-operator istio-operator 2>/dev/null) ]]; then
-  helm uninstall -n istio-operator istio-operator
+if [[ $(kubectl get applications.argoproj.io -n argocd istio-operator-istio-operator 2>/dev/null) ]]; then
+  kubectl delete applications.argoproj.io -n argocd istio-operator-istio-operator
+  helm uninstall -n istio-operator istio-operator || true
 else
   echo "Istio Operator not installed. Skipping"
 fi
--- upgrades/pre/upgrade-4-6-0.sh
+++ upgrades/pre/upgrade-4-6-0.sh
@@ -29,35 +29,35 @@ if [[ $(kubectl get deployment -n istio-operator istio-operator 2>/dev/null) ]];
 
   for crd in $(kubectl get crds -l chart=istio -o name && kubectl get crds -l app.kubernetes.io/part-of=istio -o name)
   do
-     kubectl label "$crd" "app.kubernetes.io/managed-by=Helm"
-     kubectl annotate "$crd" "meta.helm.sh/release-name=istio-base"
-     kubectl annotate "$crd" "meta.helm.sh/release-namespace=istio-system"
+     kubectl label "$crd" "app.kubernetes.io/managed-by=Helm" || true
+     kubectl annotate "$crd" "meta.helm.sh/release-name=istio-base" || true
+     kubectl annotate "$crd" "meta.helm.sh/release-namespace=istio-system" || true
   done
 
   for res in serviceaccount/istio-reader-service-account validatingwebhookconfiguration.admissionregistration.k8s.io/istiod-default-validator
   do
-     kubectl label -n istio-system "$res" "app.kubernetes.io/managed-by=Helm"
-     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-name=istio-base"
-     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-namespace=istio-system"
+     kubectl label -n istio-system "$res" "app.kubernetes.io/managed-by=Helm" || true
+     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-name=istio-base" || true
+     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-namespace=istio-system" || true
   done
 
   for res in poddisruptionbudgets.policy/istiod serviceaccount/istiod configmap/istio configmap/values configmap/istio-sidecar-injector role/istiod rolebinding/istiod service/istiod deployment/istiod horizontalpodautoscaler.autoscaling/istiod mutatingwebhookconfiguration.admissionregistration.k8s.io/istio-sidecar-injector validatingwebhookconfiguration.admissionregistration.k8s.io/istio-validator-istio-system
   do
-     kubectl label -n istio-system "$res" "app.kubernetes.io/managed-by=Helm"
-     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-name=istiod" --overwrite
-     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-namespace=istio-system"
+     kubectl label -n istio-system "$res" "app.kubernetes.io/managed-by=Helm" || true
+     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-name=istiod" --overwrite || true
+     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-namespace=istio-system" || true
   done
   for res in clusterrole/istiod-clusterrole-istio-system clusterrole/istiod-gateway-controller-istio-system clusterrole/istio-reader-clusterrole-istio-system clusterrolebinding/istiod-clusterrole-istio-system clusterrolebinding/istiod-gateway-controller-istio-system clusterrolebinding/istio-reader-clusterrole-istio-system
   do
-     kubectl label "$res" "app.kubernetes.io/managed-by=Helm"
-     kubectl annotate "$res" "meta.helm.sh/release-name=istiod"
-     kubectl annotate "$res" "meta.helm.sh/release-namespace=istio-system"
+     kubectl label "$res" "app.kubernetes.io/managed-by=Helm" || true
+     kubectl annotate "$res" "meta.helm.sh/release-name=istiod" || true
+     kubectl annotate "$res" "meta.helm.sh/release-namespace=istio-system" || true
   done
 
   for res in service/istio-ingressgateway-public serviceaccount/istio-ingressgateway-public role/istio-ingressgateway-public rolebinding/istio-ingressgateway-public deployment/istio-ingressgateway-public horizontalpodautoscaler.autoscaling/istio-ingressgateway-public
   do
-     kubectl label -n istio-system "$res" "app.kubernetes.io/managed-by=Helm"
-     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-name=istio-ingressgateway-public"
-     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-namespace=istio-system"
+     kubectl label -n istio-system "$res" "app.kubernetes.io/managed-by=Helm" || true
+     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-name=istio-ingressgateway-public" || true
+     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-namespace=istio-system" || true
   done
 fi
