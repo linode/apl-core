d423a01ca4cadd1afb61ea23ba7dc47a19b0c29b
Matthias Erll <merll@akamai.com>
2025-05-13 20:05:28 +0200

    fix: attempt without operator deinstall

--- upgrades/post/upgrade-4-6-0.sh
+++ upgrades/post/upgrade-4-6-0.sh
@@ -9,9 +9,9 @@ else
   echo "Istio Operator resource not deployed. Skipping"
 fi
 
-if [[ $(kubectl get applications.argoproj.io -n argocd istio-operator-istio-operator 2>/dev/null) ]]; then
-  kubectl delete applications.argoproj.io -n argocd istio-operator-istio-operator
-  helm uninstall -n istio-operator istio-operator || true
-else
-  echo "Istio Operator not installed. Skipping"
-fi
+#if [[ $(kubectl get applications.argoproj.io -n argocd istio-operator-istio-operator 2>/dev/null) ]]; then
+#  kubectl delete applications.argoproj.io -n argocd istio-operator-istio-operator
+#  helm uninstall -n istio-operator istio-operator || true
+#else
+#  echo "Istio Operator not installed. Skipping"
+#fi
