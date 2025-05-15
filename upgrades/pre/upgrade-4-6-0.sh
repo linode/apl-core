a0218886674afae217f5447b66ce9bdbc516d91b
Matthias Erll <merll@akamai.com>
2025-05-15 16:31:57 +0200

    feat: revised upgrade script

--- upgrades/pre/upgrade-4-6-0.sh
+++ upgrades/pre/upgrade-4-6-0.sh
@@ -31,39 +31,3 @@ if [[ ! $(kubectl get applications.argoproj.io -n argocd istio-system-istio-base
      kubectl annotate "$crd" "meta.helm.sh/release-namespace=istio-system"
   done
 fi
-
-if [[ $(kubectl get deployment -n istio-operator istio-operator 2>/dev/null) ]]; then
-  kubectl patch application -n argocd istio-operator-istio-operator --patch '[{"op": "remove", "path": "/spec/syncPolicy/automated"}]' --type=json || true
-  kubectl scale deployment -n istio-operator istio-operator --replicas=0 || true
-fi
-
-#
-#if [[ $(kubectl get deployment -n istio-operator istio-operator 2>/dev/null) ]]; then
-#
-#  for res in serviceaccount/istio-reader-service-account validatingwebhookconfiguration.admissionregistration.k8s.io/istiod-default-validator
-#  do
-#     kubectl label -n istio-system "$res" "app.kubernetes.io/managed-by=Helm" || true
-#     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-name=istio-base" || true
-#     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-namespace=istio-system" || true
-#  done
-#
-#  for res in poddisruptionbudgets.policy/istiod serviceaccount/istiod configmap/istio configmap/values configmap/istio-sidecar-injector role/istiod rolebinding/istiod service/istiod deployment/istiod horizontalpodautoscaler.autoscaling/istiod mutatingwebhookconfiguration.admissionregistration.k8s.io/istio-sidecar-injector validatingwebhookconfiguration.admissionregistration.k8s.io/istio-validator-istio-system
-#  do
-#     kubectl label -n istio-system "$res" "app.kubernetes.io/managed-by=Helm" || true
-#     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-name=istiod" --overwrite || true
-#     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-namespace=istio-system" || true
-#  done
-#  for res in clusterrole/istiod-clusterrole-istio-system clusterrole/istiod-gateway-controller-istio-system clusterrole/istio-reader-clusterrole-istio-system clusterrolebinding/istiod-clusterrole-istio-system clusterrolebinding/istiod-gateway-controller-istio-system clusterrolebinding/istio-reader-clusterrole-istio-system
-#  do
-#     kubectl label "$res" "app.kubernetes.io/managed-by=Helm" || true
-#     kubectl annotate "$res" "meta.helm.sh/release-name=istiod" || true
-#     kubectl annotate "$res" "meta.helm.sh/release-namespace=istio-system" || true
-#  done
-#
-#  for res in service/istio-ingressgateway-public serviceaccount/istio-ingressgateway-public role/istio-ingressgateway-public rolebinding/istio-ingressgateway-public deployment/istio-ingressgateway-public horizontalpodautoscaler.autoscaling/istio-ingressgateway-public
-#  do
-#     kubectl label -n istio-system "$res" "app.kubernetes.io/managed-by=Helm" || true
-#     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-name=istio-ingressgateway-public" || true
-#     kubectl annotate -n istio-system "$res" "meta.helm.sh/release-namespace=istio-system" || true
-#  done
-#fi
