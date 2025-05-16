35127703ce71bcdba7d5f6dc061bec82374bea86
Matthias Erll <merll@akamai.com>
2025-05-16 12:50:10 +0200

    fix: run istio install sooner and take ownership

--- upgrades/pre/upgrade-4-6-0.sh
+++ upgrades/pre/upgrade-4-6-0.sh
@@ -22,12 +22,3 @@ if [[ $(kubectl get applications.argoproj.io -n argocd keycloak-keycloak-operato
   kubectl delete --ignore-not-found crd keycloakrealmimports.k8s.keycloak.org
   kubectl delete --ignore-not-found crd keycloaks.k8s.keycloak.org
 fi
-
-if [[ ! $(kubectl get applications.argoproj.io -n argocd istio-system-istio-base 2>/dev/null) ]]; then
-  for crd in $(kubectl get crds -l chart=istio -o name && kubectl get crds -l app.kubernetes.io/part-of=istio -o name)
-  do
-     kubectl label "$crd" "app.kubernetes.io/managed-by=Helm"
-     kubectl annotate "$crd" "meta.helm.sh/release-name=istio-base"
-     kubectl annotate "$crd" "meta.helm.sh/release-namespace=istio-system"
-  done
-fi
