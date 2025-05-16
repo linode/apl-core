5eec366278f7c1af750294ebc6fb8967b2f25f21
Matthias Erll <merll@akamai.com>
2025-05-16 15:19:48 +0200

    feat: added uninstall as job

--- upgrades/post/upgrade-4-6-0.sh
+++ upgrades/post/upgrade-4-6-0.sh
@@ -2,8 +2,14 @@
 
 set -eu
 
-echo "Checking Istio installation."
-if [[ $(kubectl wait applications.argoproj.io -n argocd istio-system-istio-ingressgateway-public --timeout=60s --for=jsonpath='{.status.health.status}'='Healthy' 2>/dev/null) ]]; then
+if [[ $(yq '.spec.defaultRevision' "$ENV_DIR/env/apps/istio.yaml") != "1-26-0" ]]; then
+  echo "Checking Istio installation."
+  kubectl wait applications.argoproj.io -n argocd istio-system-istio-ingressgateway-public --timeout=600s --for=jsonpath='{.status.sync.status}'='Synced'
+  kubectl wait applications.argoproj.io -n argocd istio-system-istio-ingressgateway-public --timeout=600s --for=jsonpath='{.status.health.status}'='Healthy'
   echo "Istio Ingress Gateway ready. Updating revision."
   yq -I 4 -i '.spec.defaultRevision = "1-26-0"' "$ENV_DIR/env/apps/istio.yaml"
+  if [[ ! $(kubectl get job -n default istio-operator-uninstall 2>/dev/null) ]]; then
+    echo "Scheduling cleanup."
+    kubectl apply -f upgrades/post/upgrade-4-6-0-job.yaml
+  fi
 fi
