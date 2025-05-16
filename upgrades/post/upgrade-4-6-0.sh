6650360bd31bd5440102ab7fe2ebc4cd0618509f
Matthias Erll <merll@akamai.com>
2025-05-16 17:06:17 +0200

    fix: improvements to pre- and post-migration

--- upgrades/post/upgrade-4-6-0.sh
+++ upgrades/post/upgrade-4-6-0.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eu
 
-if [[ $(yq '.spec.defaultRevision' "$ENV_DIR/env/apps/istio.yaml") != "1-26-0" ]]; then
+if [ $(yq '.spec.defaultRevision' "$ENV_DIR/env/apps/istio.yaml") != "1-26-0" ]; then
   echo "Checking Istio installation."
   kubectl wait applications.argoproj.io -n argocd istio-system-istio-ingressgateway-public --timeout=600s --for=jsonpath='{.status.sync.status}'='Synced'
   kubectl wait applications.argoproj.io -n argocd istio-system-istio-ingressgateway-public --timeout=600s --for=jsonpath='{.status.health.status}'='Healthy'
   echo "Istio Ingress Gateway ready. Updating revision."
   yq -I 4 -i '.spec.defaultRevision = "1-26-0"' "$ENV_DIR/env/apps/istio.yaml"
-  if [[ ! $(kubectl get job -n default istio-operator-uninstall 2>/dev/null) ]]; then
+  if [ $(kubectl get job -ojson -n default istio-operator-uninstall | jq '.items | length') -eq 0 ]; then
     echo "Scheduling cleanup."
     kubectl apply -f upgrades/post/upgrade-4-6-0-job.yaml
   fi
