cd947a70e532067f145e7aa3c20563c273f29c8a
Matthias Erll <merll@akamai.com>
2025-05-21 15:23:21 +0200

    fix: check for pending uninstall

--- upgrades/pre/upgrade-4-7-0.sh
+++ upgrades/pre/upgrade-4-7-0.sh
@@ -5,7 +5,11 @@ set -euo pipefail
 if [ "$(yq '.spec.legacyRevision' "$ENV_DIR/env/apps/istio.yaml")" != "operator" ]; then
   echo "Checking if Istio Operator is installed."
   if [[ $(kubectl get applications.argoproj.io -n argocd istio-operator-istio-operator 2>/dev/null) ]]; then
-    echo "Operator found. Setting revision."
-    yq -I 4 -i '.spec.legacyRevision = "operator"' "$ENV_DIR/env/apps/istio.yaml"
+    if [[ -n "$(kubectl get job --ignore-not-found -ojson -n maintenance istio-operator-uninstall 2>/dev/null)" ]]; then
+      echo "Operator found. Setting revision."
+      yq -I 4 -i '.spec.legacyRevision = "operator"' "$ENV_DIR/env/apps/istio.yaml"
+    else
+      echo "Operator removal pending."
+    fi
   fi
 fi
