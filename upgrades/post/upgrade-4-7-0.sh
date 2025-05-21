14033646d82966abae4c7ba74689288ad7762c6c
Matthias Erll <merll@akamai.com>
2025-05-21 10:41:35 +0200

    fix: uninstall job namespace

--- upgrades/post/upgrade-4-7-0.sh
+++ upgrades/post/upgrade-4-7-0.sh
@@ -8,7 +8,7 @@ if [ "$(yq '.spec.defaultRevision' "$ENV_DIR/env/apps/istio.yaml")" != "1-26-0" 
   kubectl wait applications.argoproj.io -n argocd istio-system-istio-ingressgateway-public --timeout=600s --for=jsonpath='{.status.health.status}'='Healthy'
   echo "Istio Ingress Gateway ready. Updating revision."
   yq -I 4 -i '.spec.defaultRevision = "1-26-0"' "$ENV_DIR/env/apps/istio.yaml"
-  if [[ -z "$(kubectl get job --ignore-not-found -ojson -n default istio-operator-uninstall 2>/dev/null)" ]]; then
+  if [[ -z "$(kubectl get job --ignore-not-found -ojson -n maintenance istio-operator-uninstall 2>/dev/null)" ]]; then
     echo "Scheduling cleanup."
     kubectl apply -f upgrades/post/upgrade-4-7-0-job.yaml
   fi
