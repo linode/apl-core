83143cf0d3d72734e80b09a3721bb210e879a41b
Cas Lubbers <clubbers@akamai.com>
2025-09-25 16:05:40 +0200

    feat: add aiEnabled flag

--- src/common/repo.ts
+++ src/common/repo.ts
@@ -40,6 +40,7 @@ type AplKind =
   | 'AplTeamTool'
   | 'AplVersion'
   | 'AkamaiKnowledgeBase'
+  | 'AkamaiAgent'
 
 export interface FileMap {
   envDir: string
@@ -325,6 +326,16 @@ export function getFileMaps(envDir: string): Array<FileMap> {
       processAs: 'arrayItem',
       resourceGroup: 'team',
       resourceDir: 'knowledgebases',
+      loadToSpec: false,
+    },
+    {
+      kind: 'AkamaiAgent',
+      envDir,
+      jsonPathExpression: '$.teamConfig.*.agents[*]',
+      pathGlob: `${envDir}/env/teams/*/agents/*.yaml`,
+      processAs: 'arrayItem',
+      resourceGroup: 'team',
+      resourceDir: 'agents',
       loadToSpec: false,
     },
     {
