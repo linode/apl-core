ef1bfe758e8bbc8abccef4005bb0d036c334bb7a
ElderMatt <18527012+ElderMatt@users.noreply.github.com>
2025-09-26 14:47:49 +0200

    fix: removal of automated sync

--- src/common/runtime-upgrades/remove-old-minio-resources.ts
+++ src/common/runtime-upgrades/remove-old-minio-resources.ts
@@ -1,35 +1,33 @@
-import { AppsV1Api, BatchV1Api, KubeConfig } from '@kubernetes/client-node'
+import { AppsV1Api, KubeConfig } from '@kubernetes/client-node'
 import { terminal } from '../debug'
+import { k8s } from '../k8s'
 
 const kubeConfig = new KubeConfig()
 kubeConfig.loadFromDefault()
 
 const appsApi = kubeConfig.makeApiClient(AppsV1Api)
-const batchApi = kubeConfig.makeApiClient(BatchV1Api)
 
 const namespace = 'minio'
 const minioDeploymentName = 'minio'
-const minioPostJobName = 'minio-post-job'
 const minioAppName = 'minio-minio'
-const minioProvisioningJobName = 'minio-provisioning'
 
 export async function removeOldMinioResources() {
   const d = terminal('removeOldMinioResources')
   try {
     // Disable argocd auto-sync for minio application if it exists
-    await kubeConfig.makeApiClient(AppsV1Api).patchNamespacedDeployment({
-      namespace,
+    const patch = [{ op: 'remove', path: '/spec/syncPolicy/automated' }]
+    const custom = k8s.custom()
+    await custom.patchNamespacedCustomObject({
+      group: 'argoproj.io',
+      version: 'v1alpha1',
+      namespace: 'argocd',
+      plural: 'applications',
       name: minioAppName,
-      body: [
-        {
-          op: 'remove',
-          path: '/spec/syncPolicy/automated',
-        },
-      ],
+      body: patch,
     })
-    d.info('Disabled auto-sync for minio deployment.')
+    d.info('Disabled auto-sync for minio application.')
   } catch (error) {
-    d.error('Failed to disable auto-sync for minio deployment:', error)
+    d.error('Failed to disable auto-sync for minio application:', error)
   }
   try {
     // Delete Minio Deployment
