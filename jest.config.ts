8272221082034d33630bd88e8ae948360d4470fb
Cas Lubbers <clubbers@akamai.com>
2025-10-31 13:36:02 +0100

    feat: upgrade yargs, uuid and node 24

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,17 @@
-import type { Config } from '@jest/types'
-
-const config: Config.InitialOptions = {
+module.exports = {
+  preset: 'ts-jest',
   roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/src/test-init.ts'],
   moduleDirectories: ['node_modules', __dirname],
   modulePathIgnorePatterns: ['src/cmd/test.ts'],
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^yargs$': '<rootDir>/src/stubs/yargs.ts',
+    '^uuid$': '<rootDir>/src/stubs/uuid.ts',
+  },
   transform: {
-    '^.+\\.tsx?$': 'babel-jest',
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx|mjs|cjs)$': 'babel-jest',
   },
   transformIgnorePatterns: [
     'node_modules/(?!(@kubernetes/client-node|node-fetch|zx|yaml|glob|minimatch|fetch-blob|formdata-polyfill|data-uri-to-buffer|web-streams-polyfill|openid-client|oauth4webapi|jose)/)',
@@ -17,5 +20,3 @@ const config: Config.InitialOptions = {
   testMatch: ['**/*.test.ts'],
   verbose: true,
 }
-
-export default config
