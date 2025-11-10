81d2b48b61e9805032e901f43168b55c665dbad2
otomi-admin <pipeline@cluster.local>
2025-11-10 11:20:02 +0100

    fix: await

--- src/cmd/install.ts
+++ src/cmd/install.ts
@@ -67,7 +67,7 @@ const retryInstallStep = async <T, Args extends any[]>(
       retries: env.INSTALL_STEP_RETRIES,
       onRetry: async (e, attempt) => {
         d.info(`Retrying (${attempt}/${env.INSTALL_STEP_RETRIES})...`)
-        checkOperationsInProgress()
+        await checkOperationsInProgress()
       },
     },
   )
