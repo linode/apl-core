14b4813c8c19205da42e3493cfdda3ef68b55170
Ani Argjiri <aargjiri@akamai.com>
2025-11-14 09:24:38 +0100

    feat: updated post upgrade script for httpbin

--- src/common/runtime-upgrades/remove-httpbin-application.ts
+++ src/common/runtime-upgrades/remove-httpbin-application.ts
@@ -1,7 +1,7 @@
 import { terminal } from '../debug'
 import { k8s } from '../k8s'
+import { ARGOCD_APP_PARAMS } from '../constants'
 
-const namespace = 'argocd'
 const httpbinAppName = 'httpbin-httpbin'
 
 export async function removeHttpBinApplication() {
@@ -10,19 +10,15 @@ export async function removeHttpBinApplication() {
     // Delete httpbin ArgoCD application
     const custom = k8s.custom()
     await custom.deleteNamespacedCustomObject({
-      group: 'argoproj.io',
-      version: 'v1alpha1',
-      namespace,
-      plural: 'applications',
+      ...ARGOCD_APP_PARAMS,
       name: httpbinAppName,
     })
     d.info(`Deleted ${httpbinAppName} ArgoCD application.`)
   } catch (err: any) {
     if (err?.statusCode === 404 || err?.response?.statusCode === 404) {
-      d.info(`${httpbinAppName} application not found in ${namespace} namespace, skipping deletion.`)
+      d.info(`${httpbinAppName} application not found in ${ARGOCD_APP_PARAMS.namespace} namespace, skipping deletion.`)
     } else {
       d.error('Error deleting httpbin application:', err)
     }
   }
-  d.info('Successfully deleted httpbin Application')
 }
