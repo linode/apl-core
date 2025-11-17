33b616c736c97448ccb92822b0187821af1cb9e7
Cas Lubbers <clubbers@akamai.com>
2025-11-17 14:34:46 +0100

    fix: add logging

--- src/common/k8s.ts
+++ src/common/k8s.ts
@@ -371,6 +371,7 @@ export const waitTillGitRepoAvailable = async (repoUrl: string): Promise<void> =
         await $`git ls-remote ${repoUrl}`
       } catch (e) {
         d.warn(`The values repository is not yet reachable. Retrying in ${env.MIN_TIMEOUT} ms`)
+        d.error(e)
         throw e
       }
     },
