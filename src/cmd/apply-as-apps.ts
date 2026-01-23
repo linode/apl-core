242b33f371149737716dad4b08d5a61d01ef122a
Jehoszafat Zimnowoda <17126497+j-zimnowoda@users.noreply.github.com>
2026-01-23 14:33:52 +0100

    feat: limit revision history to a single one

--- src/cmd/apply-as-apps.ts
+++ src/cmd/apply-as-apps.ts
@@ -73,6 +73,7 @@ const getArgocdAppManifest = (release: HelmRelease, values: Record<string, any>,
     spec: {
       syncPolicy: ARGOCD_APP_DEFAULT_SYNC_POLICY,
       project: 'default',
+      revisionHistoryLimit: 1,
       source: {
         path: release.chart.replace('../', ''),
         repoURL: env.APPS_REPO_URL,
