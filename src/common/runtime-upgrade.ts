bd41ff186c445887bb6a18894a16a1ec6f8d5783
Matthias Erll <merll@akamai.com>
2026-03-13 21:18:53 +0100

    fix: sync check condition

--- src/common/runtime-upgrade.ts
+++ src/common/runtime-upgrade.ts
@@ -70,7 +70,7 @@ export async function runtimeUpgrade({ when, deploymentState }: RuntimeUpgradeAr
               `Runtime upgrade operations detected for version ${upgrade.version}, application: ${applicationName}`,
             )
             // In dev-mode, the app is likely not syncing in the cluster
-            if (process.env.NODE_ENV !== 'development' && applicationName === 'apl-operator-apl-operator') {
+            if (process.env.NODE_ENV !== 'development' || applicationName !== 'apl-operator-apl-operator') {
               // Wait for the ArgoCD app to be synced and healthy before running the operation
               await waitForArgoCDAppSync(applicationName, k8s.custom(), d)
               await waitForArgoCDAppHealthy(applicationName, k8s.custom(), d)
