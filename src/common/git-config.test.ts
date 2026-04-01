708e3cc4f4f99d640e6d850af83572fd52518668
Cas Lubbers <clubbers@akamai.com>
2026-04-01 15:07:50 +0200

    fix: tests

--- src/common/git-config.test.ts
+++ src/common/git-config.test.ts
@@ -166,8 +166,8 @@ describe('git-config', () => {
 
       const result = await getStoredGitRepoConfig()
       expect(result).toEqual({
-        repoUrl: 'http://gitea-http.gitea.svc.cluster.local:3000/otomi/values.git',
-        authenticatedUrl: 'http://admin:s3cret@gitea-http.gitea.svc.cluster.local:3000/otomi/values.git',
+        repoUrl: 'http://git-server.git-server.svc.cluster.local:3000/otomi/values.git',
+        authenticatedUrl: 'http://admin:s3cret@git-server.git-server.svc.cluster.local:3000/otomi/values.git',
         branch: 'main',
         email: 'pipeline@cluster.local',
         username: 'admin',
