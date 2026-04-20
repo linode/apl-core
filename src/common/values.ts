009062366ba2db0d5677e15d6a2836f348533afa
Cas Lubbers <clubbers@akamai.com>
2026-04-20 10:26:37 +0200

    feat: generate htpasswd from git username and password

--- src/common/values.ts
+++ src/common/values.ts
@@ -133,6 +133,9 @@ export const deriveSecrets = async (values: Record<string, any> = {}): Promise<R
 
   set(secrets, 'apps.harbor.registry.credentials.htpasswd', htpasswd)
 
+  const gitHtpasswd = (await $`htpasswd -nbB ${values.otomi.git.username} ${values.otomi.git.password}`).stdout.trim()
+  set(secrets, 'apps.git-server.htpasswd', gitHtpasswd)
+
   return secrets
 }
 /**
