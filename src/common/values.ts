ce238978645e8c37c3b6dd7b0bfe0e19274661f5
Jehoszafat Zimnowoda <17126497+j-zimnowoda@users.noreply.github.com>
2026-05-04 13:43:59 +0200

    fix: unused value

--- src/common/values.ts
+++ src/common/values.ts
@@ -8,7 +8,6 @@ import { $ } from 'zx'
 import { decrypt, encrypt } from './crypt'
 import { terminal } from './debug'
 import { env } from './envalid'
-import { getRepo } from './git-config'
 import { hfValues } from './hf'
 import { saveValues } from './repo'
 import {
