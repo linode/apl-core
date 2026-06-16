eb1558a96fb374cfdad06869a5ebc4c275e12591
Jehoszafat Zimnowoda <17126497+j-zimnowoda@users.noreply.github.com>
2026-06-16 15:39:09 +0200

    ci: rework

--- ci/src/release/configure-git.ts
+++ ci/src/release/configure-git.ts
@@ -5,4 +5,4 @@ const username = process.env.BOT_USERNAME!
 
 execSync(`git config --global user.email "${email}"`, { stdio: 'inherit' })
 execSync(`git config --global user.name "${username}"`, { stdio: 'inherit' })
-console.log(`Git identity configured: ${username} <${email}>`)
+console.log(`Git identity configured.`)
