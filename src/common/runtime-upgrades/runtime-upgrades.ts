3f493871f7150fefb6ac07f53f673d782e61224c
Matthias Erll <merll@akamai.com>
2026-09-11 17:04:40 +0200

    fix: recreate gitea deployments after upgrade

--- src/common/runtime-upgrades/runtime-upgrades.ts
+++ src/common/runtime-upgrades/runtime-upgrades.ts
@@ -31,6 +31,24 @@ export const runtimeUpgrades: RuntimeUpgrades = [
     pre: async ({ debug }) => {
       await stripOversizedLastAppliedAnnotations().catch((e) => debug.warn('Failed to strip oversized annotations:', e))
     },
+    post: async ({ debug }) => {
+      // Wait until all Applications have been applied, but do not wait for sync.
+      // Delete Gitea deployment and Valkey, as ArgoCD fails to update it incrementally after migrating secrets to generated ones.
+      try {
+        await k8s.app().deleteNamespacedDeployment({ name: 'gitea', namespace: 'gitea' })
+      } catch (e) {
+        if (e.response?.statusCode !== 404) {
+          debug.warn('Failed to delete Gitea deployment:', e)
+        }
+      }
+      try {
+        await k8s.app().deleteNamespacedStatefulSet({ name: 'gitea-valkey-primary', namespace: 'gitea' })
+      } catch (e) {
+        if (e.response?.statusCode !== 404) {
+          debug.warn('Failed to delete Gitea Valkey StatefulSet:', e)
+        }
+      }
+    },
     applications: {
       'istio-system-istiod': {
         post: async () => {
